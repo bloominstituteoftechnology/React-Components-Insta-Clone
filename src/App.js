db4012dd102a69c4df0c6a872169e438792af93c
Christina <chmagorton@gmail.com>
2020-01-17 16:25:18 -0600

    Updating directions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-// Start here and then work through the PostsContainer components
+/* 
+Start here and then work through the 
+PostsContainer components and the CommentSection Components. 
+Not all files need code added. 
+Look at each file to see where you need to pass props or add code 
+*/
 import React from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
