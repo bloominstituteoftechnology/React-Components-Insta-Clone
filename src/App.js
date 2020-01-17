4307c8666a2f77ab8238c9d651752bd2e7d9a216
Christina <chmagorton@gmail.com>
2020-01-17 16:16:35 -0600

    Updating instructions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+// Start here and then work through the PostsContainer components
 import React from "react";
 import "./App.css";
-// import the PostsPage and SearchBar and add them tothe App
+// import the PostsPage and SearchBar and add them to the App
+// import dummydata
+
 const App = () => {
+  // set up state for the dummy data and pass to your PostsPage
   return (
     <div className="App">
-      {/* Add components here  */}
+      {/* Add components here  and pass props where appropriate */}
     </div>
   );
 };
