6b8f34da2b012855e905b150251b59836920ed2f
Christina <chmagorton@gmail.com>
2020-01-17 15:27:51 -0600

    Updating starting code in App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import './App.css';
-import PostsPage from './components/PostsContainer/PostsPage';
-
-
-
+import React from "react";
+import "./App.css";
+// import the PostsPage and SearchBar and add them tothe App
 const App = () => {
-
-    return (
-      <div className="App">
-       <PostsPage/>
-      </div>
-    );
-  }
-
+  return (
+    <div className="App">
+      {/* Add components here  */}
+    </div>
+  );
+};
 
 export default App;
