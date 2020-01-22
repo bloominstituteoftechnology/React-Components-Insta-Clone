c972a942cc59167226be74a58244b4f3bb262971
DAP <58054581+Dlray89@users.noreply.github.com>
2020-01-22 00:26:23 -0500

    finished

---
+++ SearchBarContainer.js
@@ -0,0 +1,32 @@
+// You do not need to change any code in this file
+import React from "react";
+import "./SearchBar.css";
+
+const SearchBar = () => {
+  return (
+    <div className="search-bar-wrapper">
+      <div className="image-wrapper">
+        <i className="fab fa-instagram" />
+      </div>
+      <form className="search-form">
+        <input
+          type="text"
+          placeholder="Search"
+        />
+      </form>
+      <div className="social-wrapper">
+        <div className="social">
+          <i className="far fa-compass" />
+        </div>
+        <div className="social">
+          <i className="far fa-heart" />
+        </div>
+        <div className="social">
+          <i className="far fa-user-circle" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
---
+++ dummy-data.js
@@ -0,0 +1,50 @@
+const dummyData = [
+    {
+        username: "philzcoffee",
+        thumbnailUrl: 'https://instagram.fsjo9-1.fna.fbcdn.net/vp/d4728e507fe1eb2895fb7ca394bb6431/5E4122B1/t51.2885-15/s150x150/71118783_2434459423505735_6295784028176668008_n.jpg?_nc_ht=instagram.fsjo9-1.fna.fbcdn.net',
+        imageUrl: 'https://images.unsplash.com/photo-1477763858572-cda7deaa9bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1876&q=80',
+        likes: 400,
+        timestamp: "July 17th 2017, 12:42:40 pm",
+        comments: [
+            {
+                username: "philzcoffee",
+                text: "We've got more than just delicious coffees to offer at our shops!"
+            },
+            {
+                username: "biancasaurus",
+                text: "Looks delicious!"
+            },
+            {
+                username: "martinseludo",
+                text: "Can't wait to try it!"
+            }
+        ]
+    },
+    {
+        username: "twitch",
+        thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
+        imageUrl: 'https://images.unsplash.com/photo-1566577134770-3d85bb3a9cc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80',
+        likes: 4307,
+        timestamp: "July 15th 2017, 03:12:09 pm",
+        comments: [
+            {
+                username: "twitch",
+                text: "Epic Street Fighter action here in Las Vegas at #EVO2017!"
+            },
+            {
+                username: "michaelmarzetta",
+                text: "Omg that match was crazy"
+            },
+            {
+                username: "themexican_leprechaun",
+                text: "What a setup"
+            },
+            {
+                username: "dennis_futbol",
+                text: "It that injustice"
+            }
+        ]
+    }
+];
+
+export default dummyData;
