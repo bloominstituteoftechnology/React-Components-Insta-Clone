535ff1f268a3d0b96e952360126058c8fc8e3c82
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-01-22 13:38:07 -0700

    Import needed objects

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
+import PostsPage from "./components/PostsContainer/PostsPage";
+import SearchBar from "./components/SearchBar/SearchBarContainer";
+import dummyData from "./dummy-data";
 // import the PostsPage and SearchBar and add them to the App
 // import dummydata
 
