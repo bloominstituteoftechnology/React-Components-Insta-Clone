4ab6c10fe2f0ae0b8b47342e1b5f4446219f36a7
ksemenza27 <ksemenza27@outlook.com>
2020-01-21 19:47:19 -0500

    changed button to heart class

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -8,8 +8,8 @@ const LikeSection = props => {
       key="likes-icons-container"
     >
       <div className="like-section-wrapper">
-        <button  onClick={props.incrementLikes}>
-        <i className="far fa-heart"/></button>
+
+        <i className="far fa-heart" onClick={props.incrementLikes}/>
       </div>
       <div className="like-section-wrapper">
         <i className="far fa-comment" />
