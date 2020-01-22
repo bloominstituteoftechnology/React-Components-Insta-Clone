b09d1dcedc205a9f1bc6c18b9bdda7ba3a8ab0e1
Luis PM <luisp237@gmail.com>
2020-01-22 11:09:24 -0500

    added app.jhs useState

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
+import PostsPage from "./components/PostsContainer/PostsPage";
+import SearchBar from "./components/SearchBar/SearchBarContainer";
 // import dummydata
+import dummyData from "./dummy-data";
 
 const App = () => {
   // set up state for the dummy data and pass to your PostsPage
+  //dummyData is an array of objects
+  const [data] = useState(dummyData);
   return (
     <div className="App">
-      {/* Add components here  and pass props where appropriate */}
+       // {/* Add components here  and pass props where appropriate */}
+       <SearchBar />
+       <PostsPage Data={data}/>
+   
     </div>
   );
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,7 +4,7 @@ import Post from "./Post";
 import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = (props) => {
   return (
     <div className="posts-container-wrapper">
       {/* map through data here */}
