5def5b1d7bd60816f1f429540e4525ba29bf6757
mastaryan <sizemore.ryan@gmail.com>
2020-01-21 22:12:35 -0500

    Component Updates

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ import React from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
 // import dummydata
+import PostsPage from "./components/PostsContainer/PostsPage"
+import SearchBar from "./components/SearchBar/SearchBarContainer"
+import dummydata from "./dummy-data"
 
 const App = () => {
   // set up state for the dummy data and pass to your PostsPage
   return (
     <div className="App">
       {/* Add components here  and pass props where appropriate */}
+      <SearchBar />
+      <PostsPage data = {dummydata}/>
     </div>
   );
 };
