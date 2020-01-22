52fb139dde6b83676a1246f4ff3ea2133d75c294
Freelance Development <49110640+KejdaCelaj@users.noreply.github.com>
2020-01-22 09:51:12 -0500

    first commit

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,10 +4,13 @@ import Post from "./Post";
 import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = (props) => {
   return (
     <div className="posts-container-wrapper">
       {/* map through data here */}
+      {props.data.map((item) => (
+        <Post post={item}/>
+      ))}
     </div>
   );
 };
