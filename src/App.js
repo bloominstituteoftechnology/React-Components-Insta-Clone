68c685ecb0df18f6ced8ccdd3a14b25337d63a05
Owner <owner@Users-MacBook-Pro.local>
2020-02-02 17:58:55 -0500

    MVP reached

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
-// import dummydata
+import PostsPage from "./components/PostsContainer/PostsPage";
+import SearchBar from "./components/SearchBar/SearchBarContainer";
+import dummyData from "./dummy-data";
 
 const App = () => {
+  // console.log("here is the data", data.dummyData[0]);
   // set up state for the dummy data and pass to your PostsPage
   return (
     <div className="App">
-      {/* Add components here  and pass props where appropriate */}
+      <SearchBar />
+      <PostsPage data={dummyData} />
     </div>
   );
 };
+//Done
 
 export default App;
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,16 +1,21 @@
 //Complete the necessary code in this file
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
+import dummyData from "../../dummy-data";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+
+const PostsPage = props => {
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here */}
+      {props.data.map((item, index) => {
+        return <Post key={index} post={item} />;
+      })}
     </div>
   );
 };
 
 export default PostsPage;
 
+//Done
