92c1c807ab340abdd46f19ad44117bc649d2692e
Christina <chmagorton@gmail.com>
2020-02-13 15:10:41 -0600

    updating directions for this component

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -1,3 +1,5 @@
+// look at the likes on this component. Right now it is hard coded on line 20. 
+// console.log your props and see how to add likes to this component so that it will update when you click the icon.
 import React from 'react';
 
 const LikeSection = props => {
@@ -14,9 +16,7 @@ const LikeSection = props => {
         <i className="far fa-comment" />
       </div>
     </div>
-    <p className="like-number">
-      
-      likes</p>
+    <p className="like-number">likes</p>
 </div>
   )
 };
