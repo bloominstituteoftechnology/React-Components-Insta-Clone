53dcc35a4ff911e1659fcd9fb34ed477a4517ab7
Christina <chmagorton@gmail.com>
2020-02-17 17:08:18 -0600

    updating

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -6,21 +6,22 @@ import React from "react";
 const LikeSection = props => {
   return (
     <div>
-      <div
-        className="like-section"
-        key="likes-icons-container"
-        onClick={props.incrementLike}
-      >
-        <div className="like-section-wrapper">
-          <i className="far fa-heart" />
-        </div>
-        <div className="like-section-wrapper">
-          <i className="far fa-comment" />
-        </div>
+    <div
+      className="like-section"
+      key="likes-icons-container"
+    >
+      <div className="like-section-wrapper">
+        <i className="far fa-heart" />
       </div>
-      <p className="like-number">{props.likes} likes</p>
+      <div className="like-section-wrapper">
+        <i className="far fa-comment" />
+      </div>
     </div>
-  );
+    <p className="like-number">
+      
+      likes</p>
+</div>
+  )
 };
 
 export default LikeSection;
