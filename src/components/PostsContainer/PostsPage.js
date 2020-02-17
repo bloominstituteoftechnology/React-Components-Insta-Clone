354a44cff12e508c1fe59fb60094ccda405a6cb7
Christina <chmagorton@gmail.com>
2020-02-17 16:32:38 -0600

    Changing directions in PostsPage to reflect new data structure

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,13 +1,15 @@
 //Complete the necessary code in this file
+// import useState
 import React from "react";
 import Post from "./Post";
 import "./Posts.css";
+// import data 
 
-// pass the data from App.js down as props then map through the data
 const PostsPage = () => {
+  // set up state for your data
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here */}
+      {/* map through data here to return a Post and pass data as props to Post */}
     </div>
   );
 };
