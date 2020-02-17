282b925ed788df46c333033bd253ccad5f815a73
Christina <chmagorton@gmail.com>
2020-02-17 17:09:07 -0600

    updating

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,50 +1,39 @@
-import React, { useState } from "react";
+// You will add code in this file
+import React from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
 
 import "./Posts.css";
 
+// pass props in this file to
 const Post = props => {
-  const [likes, setLikes] = useState(
-    props.dataFromParent.likes
-  );
+  // set up state for the likes
 
-  const incrementLike = () => {
-    setLikes(likes => likes + 1);
-  };
   return (
     <div className="post-border">
       <PostHeader
-        username={
-          props.dataFromParent.username
-        }
+        username={props.post.username}
         thumbnailUrl={
-          props.dataFromParent.thumbnailUrl
+          props.post.thumbnailUrl
         }
       />
       <div className="post-image-wrapper">
         <img
           alt="post thumbnail"
           className="post-image"
-          src={props.dataFromParent.imageUrl}
+          src={props.post.imageUrl}
         />
       </div>
-      <LikeSection
-        incrementLike={incrementLike}
-        likes={likes}
-      />
+      <LikeSection />
       <CommentSection
-        postId={
-          props.dataFromParent.imageUrl
-        }
-        comments={
-          props.dataFromParent.comments
-        }
+        postId={props.post.imageUrl}
+        comments={props.post.comments}
       />
     </div>
   );
 };
 
 export default Post;
+
 
