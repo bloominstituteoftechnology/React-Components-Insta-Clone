f81bea6725a557f49e3524567bc83c63d975cfe1
JW Nicholson <john.nicholson812@gmail.com>
2020-02-18 15:35:59 -0500

    imported dummy-data into PostsPage

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,9 +4,12 @@ import React from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+import  ".../dummy-date";
 
 const PostsPage = () => {
   // set up state for your data
+  const [data] = useState()
+
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
