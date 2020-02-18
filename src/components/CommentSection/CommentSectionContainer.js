35bb514d893d82dbc76a35af9c38fe2a573ea560
ranraok <46853842+ranraok@users.noreply.github.com>
2020-02-18 17:25:58 -0500

    completed mvp

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -7,10 +7,16 @@ import "./Comment.css";
 const CommentSection = props => {
   // Add state for the comments
 
+  const [comments] = useState(props.comments);
+  console.log(props.comments);
+  
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
-      <CommentInput />
+      {comments.map(p=>(
+      <Comment comment = {p} />
+  ))}
+    <CommentInput/>
     </div>
   );
 };
