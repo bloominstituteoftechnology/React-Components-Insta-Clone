e80dc2b119736d34f98a56fe27a05da2ffd9ec87
JW Nicholson <john.nicholson812@gmail.com>
2020-02-18 16:05:18 -0500

    dummyData error fixed

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
-import dummyData from ".../dummy-data";
+import dummyData from "../../dummy-data";
 
 const PostsPage = () => {
   // set up state for your data
