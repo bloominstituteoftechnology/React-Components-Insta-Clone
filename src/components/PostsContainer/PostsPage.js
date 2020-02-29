053d26e48b612589bde636098c691816773a2e92
Alamgir <61465527+Alamgir351@users.noreply.github.com>
2020-02-29 17:22:22 -0500

    postPage update & post showing

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,18 +1,25 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+// import data
+import DummyData from "../../dummy-data";
 
 const PostsPage = () => {
   // set up state for your data
+  const [data] = useState(DummyData);
+  console.log(data);
+
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {data.map(function(p, index) {
+        // <Post key={index} post={p} />;
+        return <Post key={index} post={p} />;
+      })}
     </div>
   );
 };
 
 export default PostsPage;
-
