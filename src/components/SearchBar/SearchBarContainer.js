58ce5b0bdd132770a9c30410c918e6b6d0b00712
brianaabreu <briana.abreu01@gmail.comgit config --global user.email briana.abreu01@gmail.comgit config --global user.name brianaabreugit config --global user.email briana.abreu01@gmail.com>
2020-03-02 20:05:05 -0800

    mvp

--- src/components/SearchBar/SearchBarContainer.js
+++ src/components/SearchBar/SearchBarContainer.js
@@ -1,28 +1,29 @@
 // You do not need to change any code in this file
-import React from "react";
-import "./SearchBar.css";
+import React from 'react';
+import './SearchBar.css';
 
 const SearchBar = () => {
   return (
-    <div className="search-bar-wrapper">
-      <div className="image-wrapper">
-        <i className="fab fa-instagram" />
+    <div className='search-bar-wrapper'>
+      <div className='image-wrapper'>
+        <i className='fab fa-instagram fa-3x' />
+        <img
+          className='img'
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png'
+          alt='instagram'></img>
       </div>
-      <form className="search-form">
-        <input
-          type="text"
-          placeholder="Search"
-        />
+      <form className='search-form'>
+        <input type='text' placeholder='Search' />
       </form>
-      <div className="social-wrapper">
-        <div className="social">
-          <i className="far fa-compass" />
+      <div className='social-wrapper'>
+        <div className='social'>
+          <i className='far fa-compass' />
         </div>
-        <div className="social">
-          <i className="far fa-heart" />
+        <div className='social'>
+          <i className='far fa-heart' />
         </div>
-        <div className="social">
-          <i className="far fa-user-circle" />
+        <div className='social'>
+          <i className='far fa-user-circle' />
         </div>
       </div>
     </div>
