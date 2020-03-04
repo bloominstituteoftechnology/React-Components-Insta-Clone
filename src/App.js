8fdfbd6d45540dd4ef8446b13de692424e3ba69b
Lindsey Henager <lindseyhenager@lindseys-air.attlocal.net>
2020-03-03 20:39:12 -0800

    still working on assignment

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ Look at each file to see where you need to pass props or add code
 import React from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
-
+// import SearchBar from "../src/components/PostsContainer/PostsPage.js";
+//import {PostsPage} from "../src/components/SearchBar/SearchBarCo";
 
 const App = () => {
 
   return (
     <div className="App">
-      {/* Add imported components here to render them */}
+      {/* Add imported components here to render them */
+  
+      
+      }
     </div>
   );
 };
