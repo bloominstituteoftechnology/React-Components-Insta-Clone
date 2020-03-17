0da07a51fa86496fa425a2dfec5d940141b02d5f
Rachele Edwards <dot.com1472@gmail.com>
2020-03-17 13:34:25 -0600

    started importing and rendering in App.js folder, going through all the PostsPage and Post.js and following instructions..added script in public index.html so icons showed up

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
-// import the PostsPage and SearchBar and add them to the App
+// import the PostsPage and SearchBar and add them to the App ✅
+import PostsPage from "./components/PostsContainer/PostsPage"
+import SearchBar from "./components/SearchBar"
 
 
 const App = () => {
 
   return (
     <div className="App">
-      {/* Add imported components here to render them */}
+      {/* Add imported components here to render them */ ✅
+      <PostsPage />
+      <SearchBar />
+      }
     </div>
   );
 };
