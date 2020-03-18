5e06475a4e5264778a5aa7ee5586c552fc54ac2d
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-18 15:34:55 -0500

    created search bar

--- src/App.js
+++ src/App.js
@@ -6,14 +6,20 @@ Look at each file to see where you need to pass props or add code
 */
 import React from 'react';
 import './App.css';
+import logo from './';
+import SearchBar from './components/SearchBar/SearchBarContainer';
+import PostsPage from './components/PostsContainer/PostsPage';
+import dummyData from './dummy-data';
 // import the PostsPage and SearchBar and add them to the App
 
-console.log('hi');
 const App = () => {
 	return (
-		<div className="App">
+		<>
 			{/* Add imported components here to render them */}
-		</div>
+			<SearchBar />
+			<PostsPage />
+			{/* <PostContainer /> */}
+		</>
 	);
 };
 
