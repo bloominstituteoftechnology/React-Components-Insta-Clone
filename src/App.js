0b1f4b5047532203c2812bfd7f23166afd818477
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-18 16:20:27 -0500

    passed down props to post.js. Nothing showing yet

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import logo from './';
 import SearchBar from './components/SearchBar/SearchBarContainer';
 import PostsPage from './components/PostsContainer/PostsPage';
 import dummyData from './dummy-data';
 // import the PostsPage and SearchBar and add them to the App
 
 const App = () => {
+	const [posts] = useState(dummyData);
+
 	return (
 		<>
 			{/* Add imported components here to render them */}
 			<SearchBar />
-			<PostsPage />
+			<PostsPage parentData={posts} />
 			{/* <PostContainer /> */}
 		</>
 	);
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,18 +1,25 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
-import Post from "./Post";
-import "./Posts.css";
-// import data 
+import React, { useState } from 'react';
+import Post from './Post';
+import './Posts.css';
+import dummyData from '../../dummy-data';
+// import data
 
-const PostsPage = () => {
-  // set up state for your data
-  return (
-    <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
-    </div>
-  );
+const PostsPage = props => {
+	// set up state for your data
+
+	const [posts] = useState(dummyData);
+
+	return (
+		<div className="posts-container-wrapper">
+			{/* map through data here to return a Post and pass data as props to Post */}
+			{props.parentData.map(p => {
+				// eslint-disable-next-line no-unused-expressions
+				<Post key={p.imageUrl} dataFromParent={p} />;
+			})}
+		</div>
+	);
 };
 
 export default PostsPage;
-
