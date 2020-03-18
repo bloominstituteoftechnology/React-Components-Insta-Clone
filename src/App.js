e41c6ab5ea37886dd0c6fb6e12e9f4b75414d482
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-18 11:51:11 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 // import the PostsPage and SearchBar and add them to the App
 
-
+console.log('hi');
 const App = () => {
-
-  return (
-    <div className="App">
-      {/* Add imported components here to render them */}
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* Add imported components here to render them */}
+		</div>
+	);
 };
 
 export default App;
