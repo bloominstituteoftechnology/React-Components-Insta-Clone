cb6922c6c6863258d1f6b555bd3d4152a153b2c6
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-18 19:52:40 -0500

    likes displaying - need to add functionality

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -5,6 +5,7 @@ import React from 'react';
 
 const LikeSection = props => {
 	console.log(props);
+
 	return (
 		<div>
 			<div
--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -9,6 +9,7 @@ import './Posts.css';
 // pass props in this file to
 const Post = props => {
 	// set up state for the likes
+	console.log(props, 'props logged');
 	const [likes, setLikes] = useState(props.post.likes);
 
 	//function to increment likes
@@ -29,7 +30,7 @@ const Post = props => {
 					src={props.post.imageUrl}
 				/>
 			</div>
-			<LikeSection />
+			<LikeSection increment={incrementLike} likes={likes} />
 			<CommentSection
 				postId={props.post.imageUrl}
 				comments={props.post.comments}
