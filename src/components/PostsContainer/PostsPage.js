4ad2ddf964fdbeb44f1c1088936ed0578822f534
thayne-allen <59717714+thayne-allen@users.noreply.github.com>
2020-04-10 20:26:00 -0600

    imports state a map data for PostsPage

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -5,11 +5,18 @@ import Post from "./Post";
 import "./Posts.css";
 // import data 
 
+import dummyData from "../../dummy-data.js"
+
 const PostsPage = () => {
   // set up state for your data
+
+  const [posts] = useState(dummyData);
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {posts.map(p => (
+        <Post key={p.imageUrl} dataFromParent={p} />
+      ))}
     </div>
   );
 };
