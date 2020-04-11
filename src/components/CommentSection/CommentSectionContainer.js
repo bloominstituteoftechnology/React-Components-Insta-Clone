1a17ebcea84694239220be273ac47ae2bc51c810
thayne-allen <59717714+thayne-allen@users.noreply.github.com>
2020-04-10 20:31:50 -0600

    added state and mapped data in CommentsSectionContainer.js

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,10 +6,11 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
-
+const [comments] = useState(props.comments);
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {comments.map((comment, index) => <Comment key={index} comment={comment} />)}
       <CommentInput />
     </div>
   );
