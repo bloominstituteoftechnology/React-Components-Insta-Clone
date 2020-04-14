7f4129c6b0b99142fbf59c179d77dbf5ec818cbf
Brian Clayton <finalb@projectfinalboss.com>
2020-04-14 14:10:18 -0400

    update

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,6 +1,6 @@
 //Complete the necessary code in this file
 // import useState
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Post from "./Post";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import "./Posts.css";
@@ -10,12 +10,41 @@ import dummyData from "../../dummy-data";
 
 const PostsPage = () => {
   // set up state for your data
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    dummyData.map(data => setPosts(data));
+  }, []);
+
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
-    </div>
-  );
+ 
+ {posts.map((post, index) => {
+
+return (
+
+  <div>
+
+<Post 
+
+key={index}
+posts={post}
+CommentSection={CommentSection}
+
+/>
+
+
+  </div>
+
+    
+)})
+}   
+</div>
+);
 };
+
 
 export default PostsPage;
 
