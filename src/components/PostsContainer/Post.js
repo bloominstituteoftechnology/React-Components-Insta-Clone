839b522432ea0e14584859345651c6fbe54e99e7
manuel vega <vega1340@gmail.com>
2020-04-15 08:16:33 -0700

    finished mvp

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,5 +1,5 @@
 // You will add code in this file
-import React from "react";
+import React, {useState} from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -9,7 +9,10 @@ import "./Posts.css";
 // pass props in this file to
 const Post = props => {
   // set up state for the likes
-
+  const [likes, setLikes ] = useState(props.post.likes);
+  const increaseLikes = () => {
+    setLikes ( likes +1)
+  }
   return (
     <div className="post-border">
       <PostHeader
@@ -25,7 +28,7 @@ const Post = props => {
           src={props.post.imageUrl}
         />
       </div>
-      <LikeSection />
+      <LikeSection  increaselikes={increaseLikes} likes={likes}/>
       <CommentSection
         postId={props.post.imageUrl}
         comments={props.post.comments}
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,20 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
-
+import dummyData from "../../dummy-data"
 const PostsPage = () => {
   // set up state for your data
+  const [state] =useState(dummyData)
+  console.log(state)
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {state.map(post => {
+        return <Post post={post} key={post.id} />;
+      })}
     </div>
   );
 };
