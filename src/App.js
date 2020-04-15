051e378142d0c6c31fc5075d8060ba5c22961f89
Michael Rockingham <mrockingham2@gmail.com>
2020-04-14 20:12:28 -0400

    still working

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
+import SearchBar from './components/SearchBar/SearchBarContainer.js'
+import PostsPage from './components/PostsContainer/PostsPage'
 // import the PostsPage and SearchBar and add them to the App
 
 
 const App = () => {
+const [Post, setPost] = useState('PostsPage')
 
   return (
     <div className="App">
       {/* Add imported components here to render them */}
+      <SearchBar />
+      < PostsPage />
     </div>
   );
 };
