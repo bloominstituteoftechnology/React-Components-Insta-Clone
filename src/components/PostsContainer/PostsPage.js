a722cf924878492f168a464caa6cf1b2ea386327
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-05-22 19:59:10 -0400

    Update PostsPage.js

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,14 +4,30 @@ import React from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+//import dummyData from '../../dummy-data';
 
-const PostsPage = () => {
+const PostsPage = (props) => {
   // set up state for your data
+
   return (
+    
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+
+      {props.myData.map(item => (
+      
+        <Post
+        
+        key = {item.imageUrl}
+        datasData = {item}
+        
+        />
+      
+      ))}
+    
     </div>
+  
   );
+
 };
 
 export default PostsPage;
