b41cf3d918b73d0ac1ccf886bcf842c8c661f3cb
karlamartinezmoctez <karlamartinezmoctez@flpoly.org>
2020-05-22 00:11:21 -0400

    origin

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
+
+
 import React from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
