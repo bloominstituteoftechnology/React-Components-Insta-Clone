9c5d4d6a8c0194801aaafd6a5c26c146bb9e5766
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-05-22 19:59:08 -0400

    Update Post.js

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,5 +1,5 @@
 // You will add code in this file
-import React from "react";
+import React, {useState} from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -10,28 +10,57 @@ import "./Posts.css";
 const Post = props => {
   // set up state for the likes
 
+  const [likes, setLikes] = useState(props.datasData.likes);
+  console.log(props);
+
+  const liked = () => {
+
+    setLikes(likes => likes + 1);
+
+  };
+
   return (
     <div className="post-border">
+
       <PostHeader
-        username={props.post.username}
-        thumbnailUrl={
-          props.post.thumbnailUrl
-        }
+
+        username = {props.datasData.username}
+        thumbnailUrl = {props.datasData.thumbnailUrl}
+
       />
+
       <div className="post-image-wrapper">
+
         <img
-          alt="post thumbnail"
-          className="post-image"
-          src={props.post.imageUrl}
+
+          alt = "post thumbnail"
+          className = "post-image"
+          src = {props.datasData.imageUrl}
+
         />
+
       </div>
-      <LikeSection />
-      <CommentSection
-        postId={props.post.imageUrl}
-        comments={props.post.comments}
+
+      <LikeSection
+
+        likes = {likes}
+        liked = {liked}
+
       />
+
+      <CommentSection
+
+        postId = {props.datasData.imageUrl}
+        comments = {props.datasData.comments}
+        timeStamp={props.datasData.timestamp}
+
+      />
+
     </div>
+
   );
+  
 };
+
 
 export default Post;
