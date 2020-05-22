cbc90d04f001cc74446138c1fc239347f036480d
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-05-22 19:59:01 -0400

    Update CommentSectionContainer.js

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -3,16 +3,33 @@ import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
+import TimeStamp from './TimeStamp';
 
 const CommentSection = props => {
   // Add state for the comments
-
+  
+  //const [comment, useState] = useState(props.comments);
+  console.log('comments', props.comments);
+  
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+
+
+      {props.comments.map(item => {
+
+        return <Comment comment={item} />
+
+      })}
+
+      <TimeStamp timePost = {props.timeStamp} />
+
       <CommentInput />
+
     </div>
+
   );
+
 };
 
 export default CommentSection;
