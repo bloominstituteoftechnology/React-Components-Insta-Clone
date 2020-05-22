c47900b592ad98b0d3514b6be361246cb23f9a4b
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-05-22 19:59:13 -0400

    Update dummy-data.js

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -1,5 +1,30 @@
 const dummyData = [
     {
+        username: "supreme.ciento",
+        thumbnailUrl: 'https://scontent-lga3-1.cdninstagram.com/v/t51.2885-19/s150x150/70398851_396334221074488_1123345573827575808_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_ohc=27Id4Ete10gAX9SVhzX&oh=0559e15c2fb1fa26314460582239b0f1&oe=5EF2EDC8',
+        imageUrl: 'https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e35/70523047_177762883387701_7848907853467970160_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=109&_nc_ohc=HnIDmr6HFj4AX_ASlKx&oh=45132732d1d8e6f809f4e245f253bd78&oe=5EF0A683',
+        likes: 2100,
+        timestamp: "May 22nd 2020, 8:00:40 pm",
+        comments: [
+            {
+                username: "supreme.ciento",
+                text: "Yeah, i'm kind of a Genius."
+            },
+            {
+                username: "biancasaurus",
+                text: "wooooowwza!"
+            },
+            {
+                username: "martinseludo",
+                text: "Can't wait to also try this!"
+            },
+            {
+                username: "supreme.ciento",
+                text: "This is as far as i'll Stretch!"
+            }
+        ]
+    },
+    {
         username: "philzcoffee",
         thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
         imageUrl: 'https://images.unsplash.com/photo-1477763858572-cda7deaa9bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1876&q=80',
