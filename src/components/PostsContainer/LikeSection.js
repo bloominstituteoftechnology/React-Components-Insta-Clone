85ab871f568a082307581e2291fffca5cbbfd5bb
noah niesen <noah@noahs-MacBook-Pro.local>
2020-06-19 15:26:28 -0500

    imported PostsPage and SearchBar to App.js, finished the PostsContainer components

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -13,13 +13,13 @@ const LikeSection = props => {
         key="likes-icons-container"
       >
         <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faHeart} />
+          <FontAwesomeIcon onClick={props.incrementLikes} icon={faHeart} />
         </div>
         <div className="like-section-wrapper">
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className="like-number">27 likes</p>
+  <p className="like-number">{props.likes}</p>
     </div>
   )
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,22 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+// import data
+import dummyData from '../../dummy-data';
 
 const PostsPage = () => {
   // set up state for your data
+  const [data] = useState(dummyData);
+  console.log('dummyData', data);
+
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {data.map(p => (
+        <Post key={p.id} post={p} />
+      ))}
     </div>
   );
 };
