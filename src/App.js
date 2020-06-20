dbff877c91d6cb3e150ccdcc6ad33044c4ac6ba0
Jameslambdastudent <63371643+Jameslambdastudent@users.noreply.github.com>
2020-06-20 14:24:37 -0400

    MVP Finish

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
-
+// import dummydat
+import SearchBar from "./components/SearchBar/SearchBarContainer";
+import PostPage from "./components/PostsContainer/PostsPage";
+import dummydata from "./dummy-data";
 
 const App = () => {
+  // set up state for the dummy data and pass to your PostsPage
+  const [data] = useState(dummydata);
+  console.log(data)
 
   return (
     <div className="App">
-      {/* Add imported components here to render them */}
+      <SearchBar />
+      <PostPage postData= {data} />
     </div>
   );
 };
--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -1,27 +1,28 @@
 // look at the likes on this component. Right now it is hard coded on line 20.
 // console.log your props and see how to add likes to this component so that it will update when you click the icon.
 // HINT: You will need to add an onClick event pass through your props and update the hard coded Likes
-import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons'
+import React from 'react';
 
 const LikeSection = props => {
   return (
     <div>
-      <div
-        className="like-section"
-        key="likes-icons-container"
-      >
-        <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faHeart} />
-        </div>
-        <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faComment} />
-        </div>
+    <div
+      className="like-section"
+      key="likes-icons-container"
+    >
+      <div className="like-section-wrapper">
+        <i className="far fa-heart" onClick={props.likePost} />
       </div>
-      <p className="like-number">27 likes</p>
+      <div className="like-section-wrapper">
+        <i className="far fa-comment" />
+      </div>
     </div>
+    <p className="like-number">
+      
+      {props.likes} likes</p>
+</div>
   )
 };
 
 export default LikeSection;
+
