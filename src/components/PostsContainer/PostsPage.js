98816f81743840162941fe138a57b7d21b7769a1
Adam Osburn <wizardofosburn@gmail.com>
2020-06-22 00:24:11 -0400

    changed DummyData to data, added key to map

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,18 +4,18 @@ import React from "react";
 import Post from "./Post";
 import "./Posts.css";
 import CommentSection from '../CommentSection/CommentSectionContainer'
-import DummyData from '../../dummy-data';
+import data from '../../dummy-data';
 // import data 
 
 const PostsPage = () => {
   // set up state for your data
-  const data = DummyData;
   console.log('data in PostsPage = ', data)
   return (
     <div className="posts-container-wrapper">
       { data.map(post => {
+        console.log('post is: ', post)
         return (
-          <div>
+          <div key={`${post.username}${Date.now()}`}>
             <Post post={post} />
           </div>
         )
