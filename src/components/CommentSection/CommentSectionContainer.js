1573e09c7d6e27f475702163714b23374a92698f
Adam Osburn <wizardofosburn@gmail.com>
2020-06-22 00:29:05 -0400

    attempted to add submitComment... and changeComment functions for props and passed to components

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -7,10 +7,29 @@ import "./Comment.css";
 const CommentSection = props => {
   // Add state for the comments
   console.log('CommentSection props: ', props)
+  const {postId, comments} = props;
+  const [comment, setComment] = useState(comments)
+
+  const submitComment = (e) => {
+    e.persist();
+    setComment({...comment, [e.target.name]: e.target.value})
+  }
+  const changeComment = (e) => {
+    e.preventDefault();
+    setComment({[e.target.name]: e.target.value});
+  }
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
-      <CommentInput props={props.comments}/>
+      {comments.map(item => {
+        return (
+          <div>
+            <Comment comment={comment} setComment={setComment}/>
+          </div>
+          )
+      })
+    }
+      <CommentInput props={comments} submitComment={submitComment} changeComment={changeComment}/>
     </div>
   );
 };
