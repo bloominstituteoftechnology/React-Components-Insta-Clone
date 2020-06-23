d377ed04add29acb3dbb1f56be09ee786e475776
bhawnishk1 <mrbhawnish@gmail.com>
2020-06-23 15:08:47 -0700

    MVP complete with postPage, Likesection, Post setup

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
+import PostPage from "./components/PostsContainer/PostsPage";
+import SearchBar from "./components/SearchBar/SearchBarContainer";
 // import the PostsPage and SearchBar and add them to the App
 
 
@@ -13,7 +15,8 @@ const App = () => {
 
   return (
     <div className="App">
-      {/* Add imported components here to render them */}
+      {<SearchBar />}
+      {<PostPage />}
     </div>
   );
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,19 @@
 //Complete the necessary code in this file
 // import useState
+import { useState } from "react";
 import React from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+import dummydata from "../../dummy-data";
 
 const PostsPage = () => {
-  // set up state for your data
+  const [data, setData] = useState(dummydata);
+  console.log(data)
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {data.map(p => (
+        <Post post={p} />
+      ))}
     </div>
   );
 };
