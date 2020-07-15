7d20b5f6e321fd25c7e9d04c897d569669f752ec
Coleah Mattern <coleahmattern@gmail.com>
2020-07-15 14:02:24 -0700

    mvp I think?

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -22,9 +22,16 @@ const Post = props => {
         />
       </div>
       {/* LikeSection is missing a prop it needs to function! */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      {
+      <LikeSection 
+      likePost={() => likePost(post.id)}
+      likeCount={post.likes}
+      />
+      }
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments 
+      comments={post.comments}
+      />
     </div>
   );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,10 +5,21 @@ import "./Posts.css";
 const Posts = (props) => {
   // Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
+console.log(props)
 
   return (
     <div className="posts-container-wrapper">
       {/* map through the posts here to return a Post component */}
+            {/* map through the posts here to return a Post component */}
+        
+            {posts.map(p =>{
+          return <Post
+            key={p.id}
+            post={p}
+            likePost={likePost}
+            />
+        })
+      }
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
