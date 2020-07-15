381975278e54533b468a042d71a08d057bc546b7
imxande <alerodriguezlarosa@gmail.com>
2020-07-14 21:27:29 -0400

    comment section rendered

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -4,12 +4,17 @@ import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
-const CommentSection = props => {
+const CommentSection = (props) => {
+  console.log("CommentSection : ", props);
   // Add state for the comments
+  const [comments, setComments] = useState(props.comments);
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {comments.map((comment, index) => {
+        return <Comment key={index} comment={comment} />;
+      })}
       <CommentInput />
     </div>
   );
