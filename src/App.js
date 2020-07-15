71d87edd7e39fff1f76ce994628c425339c16c29
unknown <admin@social-collab.org>
2020-07-15 02:05:42 -0400

    Like Section

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ Look at each file to see where you need to pass props or add code
 import React from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
-
+import PostsPage from './components/PostsContainer/PostsPage'
+import SearchBar from './components/SearchBar/SearchBarContainer'
 
 const App = () => {
 
   return (
     <div className="App">
       {/* Add imported components here to render them */}
+      <SearchBar />
+      <PostsPage />
     </div>
   );
 };
