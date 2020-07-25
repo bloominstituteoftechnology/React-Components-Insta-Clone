8e3129c23211ebecaf371eb8c4f483b65847aeb5
Tony Sorensen <tony48853@gmail.com>
2020-07-25 10:28:39 -0700

    app.js importing Posts, SearchBar and dummyData correctly, dummyData is being fed to Posts

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Post from './Post';
-import './Posts.css';
+import React from "react";
+import Post from "./Post";
+import "./Posts.css";
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
 
   return (
-    <div className='posts-container-wrapper'>
+    <div className="posts-container-wrapper">
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
