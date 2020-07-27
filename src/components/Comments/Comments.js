d43d4dc28adf3bf89f95ef6240fc5933d553a49b
David Veele <you@example.com>
2020-07-27 17:58:13 -0400

    need to debug still

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import Comment from './Comment';
-import './Comments.css';
+/** @format */
 
-const Comments = props => {
-  // 🔥 Make sure the parent of Comments is passing the right props!
+import React from "react";
+import Comment from "./Comment";
+import "./Comments.css";
+
+const Comments = (props) => {
+  // Make sure the parent of Comments is passing the right props!
   const { comments } = props;
-
   return (
     <div>
-      {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map((comment) => {
+        return <Comment key={comment.id} comment={comment} />;
+      })}
     </div>
   );
 };
