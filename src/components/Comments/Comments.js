ab709980f5dc8ca55d01942e6276e5fca99ad741
unknown <abdinajibmohamud@gmail.com>
2020-08-11 16:00:15 -0700

    completed tasks

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,10 +5,26 @@ import './Comments.css';
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
-
+console.log(comments)
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {
+         comments.map(c => {
+          
+          
+          return <Comment
+           key = {c.id}
+           comment = {c}
+           
+          />
+       
+       
+       
+       })
+       
+
+      }
     </div>
   );
 };
