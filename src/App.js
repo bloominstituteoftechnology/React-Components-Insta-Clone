e37cae55765406551147443b9423cac5a6e86a1b
Mychael M <mychaelbmenges@gmail.com>
2020-08-21 14:29:30 -0600

    imported useState, dummy data, and Search bar. Rendered Search bar

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
 import './App.css';
+import postData from './dummy-data';
+import SearchBar from './components/SearchBar/SearchBar';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
+  const [data] = useState(postData);
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
@@ -31,7 +34,7 @@ const App = () => {
 
   return (
     <div className='App'>
-      {/* Add SearchBar and Posts here to render them */}
+      {<SearchBar />}
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
