86b7372478f090592685fe9aabf072ae05432366
Cassandra Villarreal <cass2u94@gmail.com>
2020-08-23 19:33:50 -0400

    I am frustrated that my array of data is not importing correctly

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Post from './Post';
 import './Posts.css';
+import App, {posts} from "/Users/cassandravillarreal/Desktop/Lambda-Work/Unit2-pt20/React-Components-Insta-Clone/public/App"
+
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
@@ -10,6 +12,7 @@ const Posts = (props) => {
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      <Post key={posts.id}/>
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
