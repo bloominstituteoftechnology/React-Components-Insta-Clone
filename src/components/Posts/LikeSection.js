a8682557f3de033e67f1918ad01fb341dd34bee0
Anthony Rodriguez <antcreatesmagic@gmail.com>
2020-08-26 17:27:50 -0500

    d

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -23,7 +23,8 @@ const LikeSection = props => {
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>100 likes</p>
+      <p className='like-number'>{numberOfLikes}</p>
+      <button onClick={likePost}>Like</button>
     </div>
   );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -6,10 +6,14 @@ const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
 
+
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {posts.map((post, index) => {
+        return <Post key={index} post={post} likePost={likePost}/>
+      })}
     </div>
   );
 };
