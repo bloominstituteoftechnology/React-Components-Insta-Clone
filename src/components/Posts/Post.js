b6b9adbfe391dcc04dc9c1464f19f146c82aa59a
Simone Ballard <simone.a.ballard@gmail.com>
2020-09-09 16:49:14 -0600

    some comment code updated

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -21,9 +21,14 @@ const Post = props => {
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      <LikeSection 
+      likePost={() => likePost(post.id)} 
+      numberOfLikes={LikeSection.length}
+      />
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments 
+      comments={Comments}
+      />
     </div>
   );
 };
