a30359a37dca04a771bb57b3f2c3962a0c5eb9fe
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-09-18 13:04:19 -0700

    imported Posts and Search bar

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, {useState} from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from './components/Posts/Posts'; 
+import SearchBar from './components/SearchBar/SearchBar'; 
 // Import the dummyData
 import './App.css';
 
