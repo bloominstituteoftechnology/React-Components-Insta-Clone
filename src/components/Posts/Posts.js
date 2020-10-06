01da2e0cf1f4e7b33a4195c62b0d6b5ecf33be03
s-munro <68410363+s-munro@users.noreply.github.com>
2020-10-06 17:50:09 -0400

    some progress, need to create like section and click event

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -3,15 +3,20 @@ import Post from './Post';
 import './Posts.css';
 
 const Posts = (props) => {
-  // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+	// 🔥 Make sure the parent of Posts is passing the right props!
+	const { likePost, posts } = props.data;
+	console.log('props', props.data);
 
-  return (
-    <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
-      {/* Check the implementation of Post to see what props it requires! */}
-    </div>
-  );
+	return (
+		<div className="posts-container-wrapper">
+			{/* Map through the posts array returning a Post component at each iteration */}
+			{props.data.map((content, index) => {
+				return <Post key={index} content={content} likePost={props.likePost} />;
+			})}
+			{/* Check the implementation of Post to see what props it requires! */
+			/* It needed a key! :) */}
+		</div>
+	);
 };
 
 export default Posts;
