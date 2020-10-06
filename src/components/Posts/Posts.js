9be03f44f849a7eda3204d09bf238eefd3b4bd05
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2020-10-06 18:16:42 -0400

    App done

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,6 +5,7 @@ import './Posts.css';
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
+  console.log(props)
 
   return (
     <div className='posts-container-wrapper'>
