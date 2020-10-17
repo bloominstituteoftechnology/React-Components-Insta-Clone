0a2aff8cbe4dc601ab4fae8f1a388e420ac6412b
Greyson <greyson.r.hamilton@gmail.com>
2020-10-17 15:27:32 -0400

    It seems to be coming along

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from './Post';
 import './Posts.css';
 
@@ -8,7 +8,11 @@ const Posts = (props) => {
 
   return (
     <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
+      {/* Map through the posts array returning a Post component at each iteration */
+      posts.map((postComp) => {
+
+        <Post post = {postComp} likePost = {likePost} />
+      })}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
