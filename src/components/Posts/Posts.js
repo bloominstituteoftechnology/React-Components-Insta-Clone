6696aed15a0d91e77ed5c91ccc9d4ef5df7e30ce
Clare Jackson <clare-jackson@lambdastudents.com>
2020-10-18 19:12:40 -0400

    Comments and onClick left to finish.

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Post from './Post';
 import './Posts.css';
+import dummyData from '../../dummy-data'
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
@@ -9,7 +10,11 @@ const Posts = (props) => {
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map((post, index) => {
+        return <Post key={index} likePost={likePost} post={post}/>
+      })}
       {/* Check the implementation of Post to see what props it requires! */}
+      
     </div>
   );
 };
