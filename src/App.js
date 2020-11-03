e69bb5b6c34b9ca7a2e15115994e871055e4de01
Diego Roman <diegormnv209@gmail.com>
2020-11-03 15:56:56 -0800

    added content to posts an fixed order of searchbar and posts

--- src/App.js
+++ src/App.js
@@ -46,9 +46,8 @@ const App = () => {
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
-      <Posts = {likePost} posts = {posts} />
       <SearchBar />
-      
+      <Posts likePost = {likePost} posts = {posts} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,22 +5,20 @@ import './Posts.css';
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
-
+  console.log('posts.js props', props);
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {
+         props.posts.map((user, upload) =>(
+           <Post key={'Posts-Map-' + upload} post={user} likePost={props.likePost} />
+         ))}
+      };
+      
     </div>
   );
 };
 
 export default Posts;
-
-
-const obj = {
-  hello:"world",
-  add:function(){
-    return "what"
-  }
-}
 
