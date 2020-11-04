a686d338464ee248f5d6447b8b176093889d82ed
juancaruizc <juancruizcruz@gmail.com>
2020-11-03 21:34:57 -0700

    mvp met

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import './App.css';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
+  const [posts, setPosts] = useState(feedDummyData);
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
@@ -30,11 +31,21 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+
+    setPosts(
+      posts.map((posts) => {
+        return posts.id === postId
+          ? { ...posts, likes: posts.likes + 1 }
+          : posts;
+      })
+    );
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar />
+      <Posts likePost={likePost} posts={posts} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -6,24 +6,21 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 
-const LikeSection = props => {
+const LikeSection = (props) => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
   const { likePost, numberOfLikes } = props;
 
   return (
     <div>
-      <div
-        className='like-section'
-        key='likes-icons-container'
-      >
+      <div className='like-section' key='likes-icons-container'>
         <div className='like-section-wrapper'>
-          <FontAwesomeIcon icon={faHeart} />
+          <FontAwesomeIcon icon={faHeart} onClick={likePost} />
         </div>
         <div className='like-section-wrapper'>
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>100 likes</p>
+      <p className='like-number'>{numberOfLikes} likes</p>
     </div>
   );
 };
