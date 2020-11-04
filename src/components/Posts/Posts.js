64cf6d4d0ba1754a6e2547427182db7d078cc0cc
Jacob Olness <jacobolness@Jacobs-MBP.lan>
2020-11-04 14:02:46 -0700

    Final

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import Post from './Post';
-import './Posts.css';
+import React from "react";
+import * as uuid from "uuid";
+import Post from "./Post";
+import "./Posts.css";
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
 
   return (
-    <div className='posts-container-wrapper'>
+    <div className="posts-container-wrapper">
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {posts.map((post) => (
+        <Post key={uuid.v4()} post={post} likePost={likePost} />
+      ))}
     </div>
   );
 };
