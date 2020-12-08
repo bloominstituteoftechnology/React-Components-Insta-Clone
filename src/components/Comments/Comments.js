b888f16338f897a36551af9a2723f66c43f76d96
justin-mavity <justinmavity1021@gmail.com>
2020-12-08 13:58:52 -0600

    finished

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import Comment from './Comment';
-import './Comments.css';
+import React from "react";
+import Comment from "./Comment";
+import "./Comments.css";
 
-const Comments = props => {
+const Comments = (props) => {
   // 🔥 Make sure the parent of Comments is passing the right props!
-  const { comments } = props;
+  const { comment } = props;
 
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comment.map((comObj) => {
+        return <Comment key={comObj.id} comments={comObj} />;
+      })}
     </div>
   );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import Post from './Post';
-import './Posts.css';
+import React from "react";
+import Post from "./Post";
+import "./Posts.css";
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+  const { likePost, post } = props;
 
   return (
-    <div className='posts-container-wrapper'>
+    <div className="posts-container-wrapper">
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {post.map((postObj) => {
+        return (
+          <Post key={postObj.id} post={postObj} likePost={() => likePost} />
+        );
+      })}
     </div>
   );
 };
