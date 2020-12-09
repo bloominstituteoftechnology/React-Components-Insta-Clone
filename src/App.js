6dc90ec3a0b2d685e1675f98ec2b116bf20c99bd
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2020-12-08 17:09:30 -0700

    completed a few sections of the project

--- src/App.js
+++ src/App.js
@@ -5,17 +5,28 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
 import './App.css';
-
+import dummyData from "./dummy-data"
+import postData from "../src/components/Posts/Posts"
+import searchBar from "../src/components/SearchBar/SearchBar"
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-
-  const likePost = postId => {
+const [posts, setPosts] = useState(postData)
+  const likePost = (postId) => {
+    
+    setPosts(
+      posts.map((post) => {
+        return postId === post.id ? { ...post, likes: post.likes +1 } : post;
+      })
+    );
+  };
+    
+    
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
@@ -27,11 +38,12 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-  };
+
 
   return (
     <div className='App'>
-      {/* Add SearchBar and Posts here to render them */}
+      <SearchBar />
+      <Posts />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -23,7 +23,7 @@ const Post = props => {
       {/* Is LikeSection getting all the props it needs to work correctly? */}
       <LikeSection likePost={() => likePost(post.id)} />
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments comments={post.comments} />
     </div>
   );
 };
