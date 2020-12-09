0383e28eb13f1140b8e7e8ac1a80d8c5c44c6f4b
Patrice Jean <73203091+PhatherJean@users.noreply.github.com>
2020-12-08 23:13:00 -0500

    Holely macrel

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import Comment from './Comment';
-import './Comments.css';
+import React from "react";
+import Comment from "./Comment";
+import "./Comments.css";
 
-const Comments = props => {
+const Comments = (props) => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
-
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map((comment) => {
+        return <Comment comment={comment} />;
+      })}
     </div>
   );
 };
