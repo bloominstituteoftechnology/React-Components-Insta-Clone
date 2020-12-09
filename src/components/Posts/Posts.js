1d68ed734b1b1a90884869bf217baa233828beed
Aleksandar Jovanovic <aleksandar04081@gmail.com>
2020-12-08 16:39:46 -0800

    React Components Instagram

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,11 +4,15 @@ import './Posts.css';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
+
   const { likePost, posts } = props;
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map(post => {
+        return <Post post = {post} likePost = {likePost}/>
+      })}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
