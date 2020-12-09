30526f40af3424b4bbc73ed56706f54eecb10735
Brennen O'Connor <brennenoconnor@Brennens-MacBook-Pro.local>
2020-12-08 20:13:24 -0500

    needs work

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -1,29 +1,26 @@
-// Look at the number of likes on line 26. Right now it's hard coded to '100'.
+// Look at the number of likes on line 26. Right now it's hard coded to '100'. (Set to numberOfLikes in JSX)
 // Use a piece of data coming in through props to display the correct number of likes.
 // You will also add an onClick handler that utilizes `likePost` to increase the count of likes.
 // (As a stretch goal, you might want to prevent your user from "liking" the same post more than once.)
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faComment, faHeart } from "@fortawesome/free-regular-svg-icons";
 
-const LikeSection = props => {
+const LikeSection = (props) => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
   const { likePost, numberOfLikes } = props;
 
   return (
     <div>
-      <div
-        className='like-section'
-        key='likes-icons-container'
-      >
-        <div className='like-section-wrapper'>
-          <FontAwesomeIcon icon={faHeart} />
+      <div className="like-section" key="likes-icons-container">
+        <div className="like-section-wrapper">
+          <FontAwesomeIcon icon={faHeart} onClick={likePost} />
         </div>
-        <div className='like-section-wrapper'>
+        <div className="like-section-wrapper">
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>100 likes</p>
+      <p className="like-number">{numberOfLikes}</p>
     </div>
   );
 };
