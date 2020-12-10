58c979a6ce4e132cae0d178bbce81966d3fea6ae
Loleatha <loleatha.joelle@gmail.com>
2020-12-10 01:08:28 -0500

    MVP done

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -6,7 +6,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 
-const LikeSection = props => {
+const LikeSection = (props) => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
   const { likePost, numberOfLikes } = props;
 
@@ -17,13 +17,13 @@ const LikeSection = props => {
         key='likes-icons-container'
       >
         <div className='like-section-wrapper'>
-          <FontAwesomeIcon icon={faHeart} />
+          <FontAwesomeIcon icon={faHeart} onClick={props.addLikes} />
         </div>
         <div className='like-section-wrapper'>
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>100 likes</p>
+      <p className='like-number'>{props.likes}</p>
     </div>
   );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -6,10 +6,21 @@ const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
 
+  console.log(posts)
+
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
-      {/* Check the implementation of Post to see what props it requires! */}
+      
+    {posts.map((postObject) => {
+      return (
+        <Post
+        likePost={likePost}
+        post={postObject}
+        />
+      ) 
+})}
+{/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
 };
