efc59ca5ed78e87ead71d22f47c9f65278e930b5
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2020-12-29 09:25:00 -1000

    committing first adjustments to app.js
    added imports, state, created slice of state, mapped posts and rendered Posts/ search bar

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
+import  dummyData  from './dummy-data.js';
+import  Posts  from "./components/Posts/Posts";
+import SearchBar from './components/SearchBar/SearchBar';
 import './App.css';
-
+const log = console.log;
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  // Debug logs to ensure properly imported
+  //log(dummyData);
+  //log(Posts);
+  //log(SearchBar);
+
+  const [posts, setPosts] = useState(dummyData);
+  const [search, setSearch] = useState("");
+  //Debug log to ensure passed through use state
+  log(posts);
 
   const likePost = postId => {
     /*
@@ -27,12 +39,25 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    setPosts(posts.map( post => {
+      if (postId === post.id){
+        log(postId);
+        return {...post, likes: post.likes + 1}
+       
+      }
+      else
+      {
+        return post;
+      }
+    }));
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <Posts likePost = { likePost } posts = { posts } />
+      <SearchBar />
     </div>
   );
 };
