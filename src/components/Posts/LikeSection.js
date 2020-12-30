50deacf63f296fa4e6c0fbb259872cf3e3500c0c
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2020-12-30 11:11:38 -1000

    like section functioning

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -1,4 +1,4 @@
-// Look at the number of likes on line 26. Right now it's hard coded to '100'.
+/ Look at the number of likes on line 26. Right now it's hard coded to '100'.
 // Use a piece of data coming in through props to display the correct number of likes.
 // You will also add an onClick handler that utilizes `likePost` to increase the count of likes.
 // (As a stretch goal, you might want to prevent your user from "liking" the same post more than once.)
@@ -9,8 +9,8 @@ import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 const LikeSection = props => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
   const { likePost, numberOfLikes } = props;
-
-  console.log(props)
+  console.log(likePost);
+  
 
   return (
     <div>
@@ -19,13 +19,14 @@ const LikeSection = props => {
         key='likes-icons-container'
       >
         <div className='like-section-wrapper'>
-          <FontAwesomeIcon icon={faHeart} />
+          <FontAwesomeIcon icon={faHeart} onClick={likePost}/>
         </div>
         <div className='like-section-wrapper'>
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>{numberOfLikes + " Likes"}</p>
+      <p className='like-number'>{likePost + ' likes'}</p>
+    
     </div>
   );
 };
