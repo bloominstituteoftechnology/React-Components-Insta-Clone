c3ac426433389d0910655ace78a5ef7cad1fe1af
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2020-12-30 11:20:03 -1000

    comments section working for mvp

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,11 +5,15 @@ import './Comments.css';
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
+  console.log(comments);
 
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
-    </div>
+    {comments.map(comment => {
+      return <Comment class="comment" key={comment.id} comment={comment} />
+    })}    
+</div>
   );
 };
 
