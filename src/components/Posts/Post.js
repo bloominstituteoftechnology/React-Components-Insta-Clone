ad4e6764ad9b35678e93c08d506073a5168bdbb5
Trevor Locke <locke.trevor@icloud.com>
2021-01-03 17:58:44 -0600

    Done

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Comments from '../Comments/Comments';
 import LikeSection from './LikeSection';
 import PostHeader from './PostHeader';
 
-const Post = props => {
+const Post = (props) => {
   // 🔥 Make sure the parent of Post is passing the right props!
-  const { post, likePost } = props;
+  const { likes, setLikes } = useState(props.post.likes)
+
+  const moreLikes = () => {
+    setLikes((likes) => likes + 1);
+  }
+
 
   return (
     <div className='post-border'>
       <PostHeader
-        username={post.username}
-        thumbnailUrl={post.thumbnailUrl}
+         username={props.post.username}
+         thumbnailUrl={props.post.thumbnailUrl}
       />
       <div className='post-image-wrapper'>
         <img
           alt='post thumbnail'
           className='post-image'
-          src={post.imageUrl}
+          src={props.post.imageUrl}
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      <LikeSection likes ={likes} moreLikes ={moreLikes} />
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments
+        postId={props.post.imageUrl}
+        comments={props.post.comments}
+        timeStamp = {props.post.timeStamp}
+      />
     </div>
   );
 };
