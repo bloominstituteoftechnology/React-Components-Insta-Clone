ce56b3a7d28e0a0d6a3b5159a25f674d08d5037e
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-01-04 10:40:24 -1000

    added comment functionality

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import Comment from './Comment';
-import './Comments.css';
+import React from "react";
+import Comment from "./Comment";
+import "./Comments.css";
 
-const Comments = props => {
+const Comments = (props) => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
-  console.log(comments);
+  const inputRef = React.useRef(null);
+
+  function handleKeyDown(event) {
+    switch (event.key) {
+      case "Enter":
+        props.onCommentEnter(props.postId);
+        inputRef.current.value = '';
+        break;
+
+      default:
+        break;
+    }
+  }
 
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
-    {comments.map(comment => {
-      return <Comment class="comment" key={comment.id} comment={comment} />
-    })}    
-</div>
+      {comments.map((comment, index) => {
+        return <Comment class="comment" key={`${index}_${comment.id}`} comment={comment} />;
+      })}
+      <input
+        type="text"
+        ref={inputRef}
+        onChange={(event) => props.onComment(event, props.postId)}
+        onKeyDown={handleKeyDown}
+      />
+    </div>
   );
 };
 
