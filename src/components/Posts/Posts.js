402969eb071f80e7076d52bf749c3bc8bf284830
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-01-04 10:41:43 -1000

    added comments functionality

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import Post from './Post';
-import './Posts.css';
+import React from "react";
+import Post from "./Post";
+import "./Posts.css";
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
-  console.log(posts)
+  const { likePost, posts, numberOfLikes } = props;
+  //console.log(posts); all posts
+  //console.log(likePost); logs postId => {
+  //console.log(numberOfLikes); undefined
 
+  //console.log(posts)
   return (
-    <div className='posts-container-wrapper'>
+    <div className="posts-container-wrapper">
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
-      {posts.map( post => {
-        return <Post likePost={likePost} post={postObject} /> 
+
+      {posts.map((postObject) => {
+        return (
+          <Post
+            key={postObject.id}
+            likePost={likePost}
+            post={postObject}
+            numberOfLikes={numberOfLikes}
+            onComment={props.onComment}
+            onCommentEnter={props.onCommentEnter}
+          />
+        );
       })}
     </div>
   );
 };
 
 export default Posts;
+
