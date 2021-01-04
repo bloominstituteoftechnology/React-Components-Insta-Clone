f979c222477fb5eaa1e1162351a4f2430f9d1c10
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-01-04 10:37:27 -1000

    stretch goals complete
    search function and add comments function working

--- src/App.js
+++ src/App.js
@@ -5,60 +5,92 @@
 */
 
 // Import the state hook
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from "./components/Posts/Posts.js";
+import Search from "./components/SearchBar/SearchBar.js";
 // Import the dummyData
-import  dummyData  from './dummy-data.js';
-import  Posts  from "./components/Posts/Posts";
-import SearchBar from './components/SearchBar/SearchBar';
-import './App.css';
-const log = console.log;
+
+import "./App.css";
+import dummyData from "./dummy-data.js";
+
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-  // Debug logs to ensure properly imported
-  //log(dummyData);
-  //log(Posts);
-  //log(SearchBar);
-
   const [posts, setPosts] = useState(dummyData);
+  const [comment, setComment] = useState({});
 
-  //Debug log to ensure passed through use state
-  log(posts);
+  const handleSearch = (event) => {
+    event.persist();
+    const { value } = event.target;
+    const filteredPosts = posts.filter((post) => {
+      return post.username.toLowerCase().includes(value.toLowerCase());
+    });
+    const postsToUse = value ? filteredPosts : dummyData;
+    setPosts(postsToUse);
+  };
 
-  const likePost = postId => {
+  const handleComment = (event, postId) => {
+    event.persist();
+    const { value } = event.target;
+    setComment((prevComment) => ({ ...prevComment, [postId]: value }));
+  };
+
+  const handlePostComment = (postId) => {
+    const toUpdate = [...posts];
+    const post = toUpdate.find((post) => post.id === postId);
+    if (post) {
+      const newComment = {
+        id: Math.floor(Math.random() * (5000 - 1) + 1),
+        username: 'tom',
+        text: comment[postId]
+      };
+      
+      post.comments.push(newComment);
+      setPosts(toUpdate);
+
+      const toUpdateComment = { ...comment };
+      delete toUpdateComment[postId];
+      setComment(toUpdateComment);
+    }
+  };
+
+  const likePost = (postId) => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
-
       The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
       This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
-
       Invoke `setPosts` and pass as the new state the invocation of `posts.map`.
       The callback passed into `map` performs the following logic:
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-    setPosts(posts.map( element => {
-      if (postId === post.id){
-       const like = element.likes + 1
-       const newPost = {...element, 'likes' : likes}
-       return newPost; 
-      }
-      else
-      {
-        return element;
-      }
-    }));
+    setPosts(
+      posts.map((element) => {
+        if (postId === postMessage.id) {
+          const likes = element.likes + 1;
+          console.log(element.likes);
+          const newPost = { ...element, likes: likes }; // does nothing
+          return newPost; //does nothing
+        } else {
+          return element;
+        }
+      })
+    );
   };
 
   return (
-    <div className='App'>
-      {/* Add SearchBar and Posts here to render them */}
-      {/* Check the implementation of each component, to see what props they require, if any! */}
-       <Search />
-       <Posts likePost = { likePost } posts = { posts } />
-     
+    <div className="App">
+      <Search onChange={handleSearch} />
+      <Posts
+        likePost={likePost}
+        posts={posts}
+        onComment={handleComment}
+        onCommentEnter={handlePostComment}
+      />
+      <h3>hello</h3>
     </div>
   );
 };
