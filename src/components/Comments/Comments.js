f6b49263cd72812a63847ed3a7e34c743007fb14
Saeed Khan <saeedkhan@Saeeds-MacBook-Pro.local>
2021-01-09 14:50:35 -0700

    completed

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -3,14 +3,17 @@ import Comment from './Comment';
 import './Comments.css';
 
 const Comments = props => {
-  // 🔥 Make sure the parent of Comments is passing the right props!
+  // ðŸ”¥ Make sure the parent of Comments is passing the right props!
   const { comments } = props;
 
   return (
     <div>
-      {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map(comment => {
+        return <Comment key={comment.id} comment={comment} />
+      })}
     </div>
-  );
-};
+  )
+  }
+    
 
 export default Comments;
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -3,13 +3,16 @@ import Post from './Post';
 import './Posts.css';
 
 const Posts = (props) => {
-  // 🔥 Make sure the parent of Posts is passing the right props!
+  // ðŸ”¥ Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
-      {/* Check the implementation of Post to see what props it requires! */}
+      {posts.map(object => {
+        return (<Post key={object.id} likePost={likePost} post={object} />)
+        {/* Check the implementation of Post to see what props it requires! */}
+      })}
     </div>
   );
 };
