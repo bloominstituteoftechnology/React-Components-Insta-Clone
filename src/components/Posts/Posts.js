3521cbd1e47547dd3ff6401e09f8a201c8ce2bd3
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-01-19 13:50:23 -0800

    everything rendering as supposed to

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,13 +5,13 @@ import './Posts.css';
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
-console.log(posts)
+
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {
         posts.map((post)=>{
-            return <Post post={post} likePost={likePost} />
+            return <Post key={post.id} post={post} likePost={likePost} />
         })
       }
       {/* Check the implementation of Post to see what props it requires! */}
