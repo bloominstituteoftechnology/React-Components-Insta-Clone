d57d5357f6fa9d21c88f1b4d7452b8e64936085c
Matthew Larson <maflarson@gmail.com>
2021-01-19 17:51:33 -0700

    got the shit working

--- src/App.js
+++ src/App.js
@@ -5,21 +5,20 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React,{useState} from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
-import Posts from './components/Posts/Posts';
+import Posts from './components/Posts/Posts'
 // Import the dummyData
-import dummyData from './dummy-data';
-import SearchBar from './components/SearchBar/SearchBar'
-import Comments from './components/Comments/Comments'
 import './App.css';
+import dummyData from './dummy-data'
+import SearchBar from './components/SearchBar/SearchBar'
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
   const [posts, setPosts] = useState(dummyData);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [term, setTerm] = useState('');
   const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
@@ -33,9 +32,10 @@ const App = () => {
         - otherwise just return the post object unchanged.
      */
     setPosts(posts.map((post) => {
-      return postId === post.id ? {...post, likes: post.likes + 1 } : post;
-    })
-    )
+      return postId === post.id
+      ? {...post, likes: post.likes + 1 }
+      : post;
+    }))
   };
 
   return (
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -9,6 +9,9 @@ const Posts = (props) => {
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map(item => {
+        return <Post post = {item} key = {item.id} likePost = {likePost}/>
+      })}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
