32867330e3caa2cf46f4f6b17ab62814bed3a804
Your Name <you@example.com>
2021-01-31 03:42:42 -0800

    worked on MVP

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, {useState} from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from './components/Posts/Posts';
+import SearchBar from './components/SearchBar/SearchBar';
 // Import the dummyData
+import dummyData from './dummy-data'
 import './App.css';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-
+  const [posts, setPosts] = useState(dummyData)
+  const [search] = useState('');
   const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
@@ -28,13 +32,20 @@ const App = () => {
         - otherwise just return the post object unchanged.
      */
   };
-
+  setPosts(posts.map(item => {
+    return item.id === postId 
+      ? {...item, likes: item.likes + 1} 
+      : item
+  })
+  );
+}
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar search={search}/>
+      <Posts likePost={likePost} posts={posts}/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
-};
 
 export default App;
