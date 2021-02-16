6e403adc00eca6f9d17adc63b2de80b8b177f9e8
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-02-16 18:00:29 -0500

    done

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import Post from './Post';
-import './Posts.css';
+import React from "react";
+import Post from "./Post";
+import "./Posts.css";
 
 const Posts = (props) => {
-  // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+	// 🔥 Make sure the parent of Posts is passing the right props!
+	const { likePost, posts } = props;
 
-  return (
-    <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
-      {/* Check the implementation of Post to see what props it requires! */}
-    </div>
-  );
+	return (
+		<div className="posts-container-wrapper">
+			{/* Map through the posts array returning a Post component at each iteration */}
+			{posts.map((post) => {
+				return <Post key={post.id} likePost={likePost} post={post} />;
+			})}
+
+			{/* Check the implementation of Post to see what props it requires! */}
+		</div>
+	);
 };
 
 export default Posts;
