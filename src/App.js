83e34f7218c13e35dc8fb3a870f4f812f3a598a1
MatthewEllisTx <mbellistx@gmail.com>
2021-02-17 17:49:37 -0600

    done

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
 import './App.css';
+import Posts from './components/Posts/Posts.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
+import dummyData from './dummy-data.js';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-
+  const [posts, setPosts] = useState(dummyData.map( post => { return {...post, liked: false}}));
+  // const [searchBar, setSearchBar] = useState(SearchBar);
   const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
@@ -27,11 +31,23 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    
+    //setPosts(posts.map( post => post.id === postId ? {...post, likes: post.likes + change} : post));
+    setPosts(posts.map( post => {
+      if(post.id === postId){
+        const change = post.liked ? -1 : 1;
+        post.liked = !post.liked;
+        return {...post, likes: post.likes + change};
+      }
+      return post;
+    }));
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar/>
+      <Posts likePost={likePost} posts={posts}/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
