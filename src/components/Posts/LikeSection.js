014c9f8a92e624ed5f46491d2b2ee1a8d04fe2be
nickcabuno <nickcabuno17@gmail.com>
2021-02-16 22:25:41 -0500

    instagram

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -7,7 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 
 const LikeSection = props => {
-  // 🔥 Make sure the parent of LikeSection is passing the right props!
+  // Fire Make sure the parent of LikeSection is passing the right props!
   const { likePost, numberOfLikes } = props;
 
   return (
@@ -16,14 +16,14 @@ const LikeSection = props => {
         className='like-section'
         key='likes-icons-container'
       >
-        <div className='like-section-wrapper'>
+        <div onClick={likePost} className='like-section-wrapper'>
           <FontAwesomeIcon icon={faHeart} />
         </div>
         <div className='like-section-wrapper'>
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>100 likes</p>
+      <p className='like-number'>Likes: {numberOfLikes}</p>
     </div>
   );
 };
