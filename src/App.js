dfd5b25840cc7bc29689dd610173260cb3a0f471
josh jacques <jp.jacques@yahoo.com>
2021-02-27 15:24:14 -0600

    still a bit confused on how all of it works..just have comments left

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
-/* 
-  Start here and work your way down the nested components.
-  Not all files in the project need code added.
-  Look at each file to see what props need to be passed!
+/*
+Start here and work your way down the nested components.
+Not all files in the project need code added.
+Look at each file to see what props need to be passed!
 */
 
 // Import the state hook
-import React from 'react';
+//
+import React, { useState } from 'react';
+
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
+import Posts from './components/Posts/Posts.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
+import dummyData from './dummy-data.js';
 import './App.css';
+console.log(dummyData)
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
+  const [posts, setPosts] = useState(dummyData);
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [search, setSearch] = useState('');
 
   const likePost = postId => {
     /*
-      This function serves the purpose of increasing the number of likes by one, of the post with a given id.
+    This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
-      The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
-      This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
+    The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
+    This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
 
-      Invoke `setPosts` and pass as the new state the invocation of `posts.map`.
-      The callback passed into `map` performs the following logic:
-        - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
-        - otherwise just return the post object unchanged.
-     */
+    Invoke `setPosts` and pass as the new state the invocation of `posts.map`.
+    The callback passed into `map` performs the following logic:
+    - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
+    - otherwise just return the post object unchanged.
+    */
+    setPosts(posts.map(post => {
+      return post.id === postId ? { ...post, likes: post.likes + 1 } : post;
+    }));
   };
 
   return (
-    <div className='App'>
-      {/* Add SearchBar and Posts here to render them */}
-      {/* Check the implementation of each component, to see what props they require, if any! */}
-    </div>
+    <div className = 'App'>
+    <SearchBar / >
+    <Posts likePost={likePost} posts={posts} />
+    {/* Check the implementation of each component, to see what props they require, if any! */ }
+  < /div>
   );
 };
 
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -8,8 +8,14 @@ const Posts = (props) => {
 
   return (
     <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
-      {/* Check the implementation of Post to see what props it requires! */}
+    {posts.map(post => {
+      return <Post
+      post = { post }
+      likePost = { likePost }
+      key = { post.id }//don't really understand this
+      />;
+    })/* Map through the posts array returning a Post component at each iteration */}
+    {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
 };
