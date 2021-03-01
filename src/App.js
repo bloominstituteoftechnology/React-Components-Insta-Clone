6dece11b662877728cfed9f8781bc1e6e999419d
yonathan Admasu <admyoni4@yahoo.com>
2021-02-28 20:13:38 -0800

    imported some componenets and worked on app.js

--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@
 
 // Import the state hook
 import React from 'react';
+import {useState} from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
 import './App.css';
+import Posts from './components/Posts/Posts';
+import posts from './components/Posts/Posts';
+import SearchBar from './components/SearchBar/SearchBar.js';
+import dummyData from './dummy-data';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+    const [posts, setPosts] = useState(dummyData);
+    const [searchBar, setSearchBar] = useState('')
 
   const likePost = postId => {
     /*
@@ -27,11 +34,35 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    
+    // const newPost = posts.map(likes => {
+    //   if (likes.id === postId){
+    //   return {...likes, likes: likes.likes + 1}
+    //   } else return likes
+    //   }
+    //   setPosts(newPost)
+      //or with ternary
+      setPosts(posts.map(post => {
+        return post.id === postId ? { ...post, likes: post.likes + 1 } : post
+      }))
+      
+    
+  
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar 
+      SearchBar = {SearchBar}
+      />
+       <Posts
+         post = {posts}
+         Postsprop = {posts}
+         />
+      
+    
+     
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
