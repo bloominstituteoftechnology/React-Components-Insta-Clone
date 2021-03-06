5601626e8683c97be3feeff622bc0e5b20cc7a7f
yonathan Admasu <admyoni4@yahoo.com>
2021-03-05 22:04:52 -0800

    fixed some bugs in my codes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import {useState} from 'react';
 // Import the dummyData
 import './App.css';
 import Posts from './components/Posts/Posts';
-import posts from './components/Posts/Posts';
 import SearchBar from './components/SearchBar/SearchBar.js';
 import dummyData from './dummy-data';
 
@@ -20,7 +19,7 @@ const App = () => {
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
     const [posts, setPosts] = useState(dummyData);
-    const [searchBar, setSearchBar] = useState('')
+  
 
   const likePost = postId => {
     /*
@@ -37,7 +36,7 @@ const App = () => {
     
     // const newPost = posts.map(likes => {
     //   if (likes.id === postId){
-    //   return {...likes, likes: likes.likes + 1}
+    //   return {...post, likes: post.likes + 1}
     //   } else return likes
     //   }
     //   setPosts(newPost)
@@ -53,14 +52,8 @@ const App = () => {
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
-      <SearchBar 
-      SearchBar = {SearchBar}
-      />
-       <Posts
-         post = {posts}
-         Postsprop = {posts}
-         />
-      
+      <SearchBar />
+      <Posts posts = {posts} likePost = {likePost}/>
     
      
       {/* Check the implementation of each component, to see what props they require, if any! */}
