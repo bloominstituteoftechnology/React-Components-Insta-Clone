b597a3aa19fd590a036f8d87d06351a47ee1e5be
Daniel-Walker <Dw765608@gmail.com>
2021-03-25 16:53:26 -0500

    finished

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -9,9 +9,19 @@ const Posts = (props) => {
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map(Object => {
+        return (
+          <Post
+            key={Object.id}
+            likePost={likePost}
+            post={Object}
+            numberOfLikes={Object.likes}
+            comments={Object.comments}
+          />
+        )
+      })}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
 };
-
 export default Posts;
