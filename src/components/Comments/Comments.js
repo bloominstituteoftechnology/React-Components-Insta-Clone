1a64d4fdfb7daa2caa0851363e5fb6c3befd5ce6
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-03-26 11:38:26 -0400

    next ill research passing functions as props

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -9,7 +9,7 @@ const Comments = props => {
   return (
     <div>
       {comments.map(comment => {
-        return <Comment comment={comment}/>
+        return <Comment comment={comment} key={comment.id}/>
       })}
       {/* map through the comments prop and render a Comment for every piece of data */}
     </div>
