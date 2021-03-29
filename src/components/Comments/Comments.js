1964e6117b32a676f3ad60b0c53dc99c09fd444f
Bus42 <gbrewton1980@gmail.com>
2021-03-28 22:23:08 -0600

    Completes MVP

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -3,12 +3,11 @@ import Comment from './Comment';
 import './Comments.css';
 
 const Comments = props => {
-  // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
 
   return (
     <div>
-      {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map(comment => <Comment comment={comment} />)}
     </div>
   );
 };
