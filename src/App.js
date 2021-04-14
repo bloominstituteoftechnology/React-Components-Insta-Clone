bbd74f49a1b8b0e5f0f2dc0f858116df0a468f28
cynthiaannberg <cynthia.ann.berg@me.com>
2021-04-14 00:32:45 -0600

    react

--- src/App.js
+++ src/App.js
@@ -9,12 +9,16 @@ import React from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
 import './App.css';
+import Posts from "./components/Posts/Posts";
+import SearchBar from "./compnents/SearchBar/SearchBar";
+import dummyData from "./dummy-data";
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
+  const [posts, setPosts] = useState(dummyData);
   const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
@@ -27,12 +31,20 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    setPosts(posts.map(Item) => {
+      if(item.id === postId){
+        return {...item, likes: item.likes +1}
+      }
+      return posts
+    }))
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <Posts likePost={likePost} posts={posts}/>
+      <SearchBar />
     </div>
   );
 };
--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -9,6 +9,10 @@ const Comments = props => {
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map((commet) => {
+        return <Comment comment={comment} key={comment.id}/>
+      })
+      }
     </div>
   );
 };
