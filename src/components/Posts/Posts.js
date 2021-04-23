729ed9b9eefae24453ec61aeed9c7beeebd71854
Kendell-Ely <kendell.c.ely@outlook.com>
2021-04-23 12:12:13 -0400

    posts complete

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -8,7 +8,13 @@ const Posts = (props) => {
 
   return (
     <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
+      {/* Map through the posts array returning a Post component at each iteration */
+      posts.map((post)=> {
+        return (
+          <Post key = {post.id} post = {post} likePost = {post}/>
+        )
+      })
+}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
