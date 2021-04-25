0337a45fb704ccf277f53cae14614f8443c804bf
josh jacques <jp.jacques@yahoo.com>
2021-04-25 13:28:23 -0500

    just have to finish likes

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -6,10 +6,12 @@ const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
 
+
   return (
     <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
+      { /* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {props.props.map(e => <Post post={e} />)}
     </div>
   );
 };
