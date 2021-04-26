9f48a38a8748073d32763f8a6a3365997c5e7bbc
Owen  Damron <damronowen@yahoo.com>
2021-04-25 23:58:00 -0500

    fixed map on app.js and finished comments.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,8 @@
 import React, {useState} from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
-import PostHeader  from './/components//Posts/PostHeader'
-import Post from './/components//Posts/Post'
-import Posts from './/components//Posts/Posts'
-import LikeSection from './/components//Posts/LikeSection'
-// import SearchBar from './/components///SearchBar'
+import Posts from './components/Posts/Posts'
+import SearchBar from './components/SearchBar/SearchBar'
 import dummyData from './dummy-data'
 import './App.css';
 
@@ -21,14 +18,10 @@ const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+
 const [posts, setPosts] = useState(dummyData)
-// const [search, setSearch] = useState(SearchBar)
-  const likePost = (id, posts) => { 
-    const newLikeData = posts.map(post => post.id == id ? {...post, likes: post.likes+1} : post)
-    setPosts(newLikeData)
 
-
-    /*
+/*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
       The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
@@ -39,13 +32,17 @@ const [posts, setPosts] = useState(dummyData)
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+
+  const likePost = postId => { 
+    setPosts(posts.map((post) => {
+     return post.id === postId ? {...post, likes: post.likes+1} : post
+    })) 
   };
 
   return (
     <div className='App'>
-      {/* Add SearchBar and Posts here to render them */}
-      {/* Check the implementation of each component, to see what props they require, if any! */}
-      {/* <Search /> */}
+      
+      <SearchBar />
       <Posts posts={posts} likePost={likePost}/>
     </div>
   );
