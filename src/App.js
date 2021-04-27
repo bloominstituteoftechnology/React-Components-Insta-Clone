58d9c2d65f4e9cb6fdd1955675af78790d5f094d
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-04-27 11:31:02 -0500

    looking at the readme to find out what i need to do next

--- src/App.js
+++ src/App.js
@@ -35,13 +35,22 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-    
+    setPosts(posts.map(post=>{
+     if (postId == id){
+       post.likes = post.likes + 1
+       return post
+     }
+     return post
+    }))
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar search = {search}/>
+      <Posts posts = {posts}/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
+
     </div>
   );
 };
