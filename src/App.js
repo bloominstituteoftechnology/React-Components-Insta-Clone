2ad0d820375ea297f15b04acf46e0de70b7f6c50
vida <ofo28@aol.com>
2021-05-24 02:26:47 -0400

    intial push

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ const App = () => {
       {/* Add SearchBar and Posts here to render them */}
       <SearchBar/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
-      <Posts likePost={likePost} posts={posts}/>
+     // /* <Posts likePost={likePost} posts={posts}/>*/
      
     </div>
   );
