d9edca8222804e8f44a29af136cc54e9033e686e
trevorlecroy <trevorlecroy@hotmail.com>
2021-06-20 18:44:18 -0400

    Finished Project. User can only like a post once, and click again to unlike the post.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, {useState} from 'react';
+import Posts from './components/Posts/Posts';
+import SearchBar from './components/SearchBar/SearchBar';
+import dummyData from './dummy-data';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
 import './App.css';
@@ -14,8 +17,9 @@ const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts, setPosts]     = useState(dummyData);
 
-  const likePost = postId => {
+  const likePost = (postId, isUnlike) => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
@@ -27,11 +31,28 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+
+    let increment = isUnlike ? -1 : 1;
+
+    setPosts(
+      posts.map(post => {
+        if(post.id == postId) {
+          return {
+            ...post,
+            likes: post.likes + increment
+          }
+        } else {
+          return post;
+        }
+      })
+    );
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar/>
+      <Posts likePost={likePost} posts={posts} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Comments from '../Comments/Comments';
 import LikeSection from './LikeSection';
 import PostHeader from './PostHeader';
 
 const Post = props => {
   // 🔥 Make sure the parent of Post is passing the right props!
-  const { post, likePost } = props;
+  const { post, likePost }    = props;
+  const [isLiked, setIsLiked] = useState(false);
 
   return (
     <div className='post-border'>
@@ -21,9 +22,19 @@ const Post = props => {
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      <LikeSection likePost={() => { 
+        if(isLiked) {
+          likePost(post.id, true);
+          setIsLiked(false);
+          return;
+        }
+
+        likePost(post.id, false) 
+        setIsLiked(true);
+      }} 
+          numberOfLikes={post.likes} />
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments comments={post.comments} />
     </div>
   );
 };
