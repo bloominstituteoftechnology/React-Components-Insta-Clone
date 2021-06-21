66beed7c9c85fe2f2cbdcf8570996c92a983daf5
Justin Bain <jbain912@gmail.com>
2021-06-20 22:38:37 -0500

    Done

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -9,7 +9,7 @@ const Posts = (props) => {
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
-      {/* Check the implementation of Post to see what props it requires! */}
+      {posts.map(post => (<Post post={post} likePost={likePost}/>))}
     </div>
   );
 };
