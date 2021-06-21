95cdac7ae2a969ace1f73c9b54daf35c85f8b4ad
Dalian <DalianAlyxander@me.com>
2021-06-21 00:20:23 -0400

    missing onclick for like button otherwise  met mvp

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
+import Posts from './components/Posts/Posts'
+import SearchBar from './components/SearchBar/SearchBar'
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
+import dummyData from './dummy-data'
 import './App.css';
 
 const App = () => {
@@ -15,7 +18,17 @@ const App = () => {
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
-  const likePost = postId => {
+  const [posts, setPosts] = useState(dummyData)
+
+  const likePost = (postId) => {
+    setPosts(
+      posts.map((friend) => {
+        if (friend.id === postId) {
+          return { ...friend, likes: friend.likes+1}
+        } else return friend;
+      })
+    )
+
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
@@ -33,6 +46,8 @@ const App = () => {
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <SearchBar SearchBar={SearchBar} />
+      <Posts posts={posts} likePost={likePost}/>
     </div>
   );
 };
