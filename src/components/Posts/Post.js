1ae1befab9e7ffe9a9c5ba277cfc9818eb3d7a78
TrevourGuild <trevorr.guildd@gmail.com>
2021-07-13 13:25:33 -0700

    finished the project

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -6,7 +6,7 @@ import PostHeader from './PostHeader';
 const Post = props => {
   // 🔥 Make sure the parent of Post is passing the right props!
   const { post, likePost } = props;
-
+  console.log(post)
   return (
     <div className='post-border'>
       <PostHeader
@@ -23,7 +23,7 @@ const Post = props => {
       {/* Is LikeSection getting all the props it needs to work correctly? */}
       <LikeSection likePost={() => likePost(post.id)} />
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments comment = {post.comment}/>
     </div>
   );
 };
