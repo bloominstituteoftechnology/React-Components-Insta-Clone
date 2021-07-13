2295c9397227f14e9273e5c80bcc55fec3166ea8
Andrewschaer <andrewschaer@github.com>
2021-07-13 18:39:57 -0400

    Completed all updates to component files

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ const App = () => {
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
       <SearchBar />
-      <Posts Posts = {posts} LikePost = {likePost}/>
+      <Posts likePost = {likePost} posts = {posts} />
     </div>
   );
 };
