8699b8f429739c82993ce7f0c11a1f3e81f83c68
KRQuinn <kr.quinn82@gmail.com>
2021-07-13 20:22:06 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@
 // Import the state hook
 import React from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from './components/Posts/Posts';
 // Import the dummyData
+import dummyData from './dummy-data';
 import './App.css';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts, setPosts] = useState(dummyData);
 
   const likePost = postId => {
     /*
@@ -27,6 +30,9 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    setPosts(posts.map(post => {
+      return post.id===postId ? {} : post;
+    }))
   };
 
   return (
