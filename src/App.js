6ce6f3c981911e974852604beecd0f21d4bdd934
Rick <rmansfield@realpeoplesratings.com>
2021-07-14 09:03:26 -0500

    U2.2M2 Homework renderings added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@
 */
 
 // Import the state hook
-import React {useState} from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
-import LikeSection from './components/Posts/LikeSection';
+// import LikeSection from './components/Posts/LikeSection';
 import Posts from './components/Posts/Posts';
-import PostHeader from './components/Posts/PostHeader';
-import Post from './components/Posts/Post';
+// import PostHeader from './components/Posts/PostHeader';
+// import Post from './components/Posts/Post';
 import SearchBar from './components/SearchBar/SearchBar';
 // Import the dummyData
 import dummyData from './dummy-data';
@@ -18,8 +18,11 @@ import './App.css';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
+  const [posts, setPosts] = useState(dummyData);
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
+
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [search, setSearch] = useState('');
 
   const likePost = postId => {
     /*
@@ -33,12 +36,25 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    setPosts(posts.map(postObj =>{
+      if(postObj.id === postId){
+        return{...postObj, likes: postObj.likes +1};
+      } else {
+        return postObj;
+      }
+    }));
+  
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      {/* <LikeSection likePost={likePost}/> */}
+      <Posts post={posts} likePost={likePost}/>
+      {/* <PostHeader />
+      <Post /> */}
+      <SearchBar />
     </div>
   );
 };
