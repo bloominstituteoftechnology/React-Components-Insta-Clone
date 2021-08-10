3908a1f0016e37d267f91debd817aada1a0fdf8b
BrendonHawkins <artbitcodedata@gmail.com>
2021-08-10 18:14:00 -0400

    somewhat decent

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,16 +2,28 @@ import React from 'react';
 import Post from './Post';
 import './Posts.css';
 
-const Posts = (props) => {
+//Define the Posts function, pass in props
+const Posts = (props) => 
+{
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
 
+  //Return function defined
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      
+      { //Map over posts, extract the post info
+        posts.map((postInfo) => 
+        {
+          //Return the post and like info
+          return Post({post:postInfo, likePost:likePost});
+        })
+      }
     </div>
   );
 };
 
+//Export statement
 export default Posts;
