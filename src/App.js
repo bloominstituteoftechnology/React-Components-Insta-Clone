fc767c5a8702789e30103d9dfdd3bbfaaf0873c8
Brent Besselievre <brent@Brents-MacBook-Pro.local>
2021-08-10 13:42:10 -0600

    Finished MVP

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ const App = () => {
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       <SearchBar />
-      <Posts />
+      <Posts posts={posts} likePost={likePost}/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
