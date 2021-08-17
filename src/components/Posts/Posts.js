790fddac0bb3c20c44ccc409987d01cdd40d0089
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-08-16 18:01:36 -0700

    warnings

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Post from "./Post";
+// import Post from "./Post";
 import "./Posts.css";
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+  // const { likePost, posts } = props;
 
   return (
     <div className="posts-container-wrapper">
