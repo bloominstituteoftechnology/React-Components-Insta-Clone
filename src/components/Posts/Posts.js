c44dd23edd78808e42402257c40ed6d0a66c7e28
Kurt La Fond <kurtlafond@gmail.com>
2021-09-07 22:12:11 -0700

    insta clone

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import Post from './Post';
 import './Posts.css';
+
+import Post from './Post';
+import React from 'react';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+  const { likePost, posts } = props; 
 
   return (
     <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
-      {/* Check the implementation of Post to see what props it requires! */}
+      {posts.map((post) => {
+      return <Post key={post.id} likePost={likePost} post={post} />
+      })}
     </div>
   );
 };
