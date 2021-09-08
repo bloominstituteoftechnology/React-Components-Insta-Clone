b3c1f44e243ce43603c77ecc5f232d45e90fadff
Matthew Stack <matthewstack@Matthews-MacBook-Pro-4.local>
2021-09-07 21:37:24 -0400

    finished mvp

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -4,11 +4,14 @@ import './Comments.css';
 
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
-  const { comments } = props;
+  const { comments, post } = props;
 
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      { post.comments.map(comment => {
+        return <Comment key={comments.id} comment={comment} />
+      })}
     </div>
   );
 };
--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -21,9 +21,9 @@ const Post = props => {
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      <LikeSection numberOfLikes={post.likes} likePost={() => likePost(post.id)} post={post}/>
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments comments={post.comments} post={post}/>
     </div>
   );
 };
