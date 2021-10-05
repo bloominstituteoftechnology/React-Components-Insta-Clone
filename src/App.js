d8d00676057f3424f3ebf4c91598e5612522f896
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-10-05 13:47:34 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
-  const likePost = postId => {
+  //const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
@@ -27,7 +27,7 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-  };
+  //};
 
   return (
     <div className='App'>
