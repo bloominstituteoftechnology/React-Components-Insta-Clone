d180364242f506902ca2fb9258c8db01a7a8c9a1
brad <bradley.michaels@outlook.com>
2021-10-05 18:40:49 -0400

    cleaning up some code

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -24,7 +24,7 @@ const LikeSection = props => {
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>{numberOfLikes}</p>
+      <p className='like-number'>{numberOfLikes} likes</p>
     </div>
   );
 };
