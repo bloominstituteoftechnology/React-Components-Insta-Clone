cc8545a9d65b1dba36dd4089e407eb289bd2ef2f
Peter Conley <peterdavidconley@gmail.com>
2021-10-05 14:26:45 -0400

    rendered posts to DOM

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@
   Not all files in the project need code added.
   Look at each file to see what props need to be passed!
 */
-import SearchBar from './components/SearchBar/SearchBar'
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import SearchBar from './components/SearchBar/SearchBar'
+import Posts from './components/Posts/Posts'
+
 // Import the dummyData
 import './App.css';
+import dummyData from './dummy-data'
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
+
+  const [ posts, setPosts ] = useState(dummyData);
+
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+
+  const [ searchFunc, setSearchFunc ] = useState('');
 
   const likePost = postId => {
     /*
@@ -34,6 +42,7 @@ const App = () => {
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       < SearchBar />
+      < Posts posts={posts} likePost={likePost}/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Post from './Post';
 import './Posts.css';
 
+
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
@@ -9,6 +10,9 @@ const Posts = (props) => {
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      { posts.map( post => {
+        return < Post post={post}/>
+      })}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
