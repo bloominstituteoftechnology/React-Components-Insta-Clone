fa1a5717e31937d5efbf37456797f476823c5b65
Cheyenne Bowman <cheyennebowman1212123@gmail.com>
2021-10-05 16:00:31 -0500

    done

--- src/App.js
+++ src/App.js
@@ -5,17 +5,31 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from './components/Posts/Posts'
+import SearchBar from './components/SearchBar/SearchBar';
 // Import the dummyData
 import './App.css';
-
+import dummyData from './dummy-data'
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts, setposts] = useState(dummyData)
 
   const likePost = postId => {
+      const updatedPost = posts.map(post =>{
+        if ( post.id === postId) {
+          const newLikes = post.likes + 1
+          const newPost = {...post, likes: newLikes}
+          return newPost
+        } else {
+          return post
+        }
+      })
+  
+    setposts(updatedPost)
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
@@ -32,6 +46,7 @@ const App = () => {
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <Posts posts={posts} likePost={likePost}/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -4,6 +4,7 @@ import React from 'react';
 const Comment = props => {
   // 🔥 Make sure the parent of Comment is passing the right props!
   const { comment } = props;
+  console.log('INSIDE COMMENT', comment)
 
   return (
     <div className='comment-text'>
