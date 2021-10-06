6909c7695266e0172162b5f374687f52b2cae76f
Darla Young <me3u2@hotmail.com>
2021-10-06 00:25:35 -0500

    posts complete?

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Post from './Post';
 import './Posts.css';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
-
+  const {post, setPost} = useState(posts.id);
+  
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {}
+      {posts.map(() => <div>{post}</div>)}
       {/* Check the implementation of Post to see what props it requires! */}
+      <Posts posts={posts} likePost={likePost} />
+      <Post post={post} />
     </div>
   );
 };
