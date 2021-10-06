bd09aea6ac9d789c4a88e860d25e87b7bcab28e0
hmzsjd <github@hamzasajid.com>
2021-10-05 20:37:48 -0700

    MVP complete

--- src/App.js
+++ src/App.js
@@ -5,17 +5,36 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
 import './App.css';
+import Posts from './components/Posts/Posts';
+import SearchBar from './components/SearchBar/SearchBar';
+import dummyData from './dummy-data';
+
+
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts, setPosts] = useState(dummyData);
+
+
+
 
   const likePost = postId => {
+    setPosts(posts.map(post => {
+      if (post.id === postId) {
+        return { ...post, likes: post.likes+1};
+      } else {
+        return post;
+      }
+    }));
+
+
+
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
@@ -31,8 +50,9 @@ const App = () => {
 
   return (
     <div className='App'>
-      {/* Add SearchBar and Posts here to render them */}
+      {<SearchBar />}
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <Posts likePost={likePost} posts={posts}/>
     </div>
   );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -8,7 +8,9 @@ const Posts = (props) => {
 
   return (
     <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map((post,idx) => {
+        return <Post post={post} likePost={likePost} key={idx}/>
+      })}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
