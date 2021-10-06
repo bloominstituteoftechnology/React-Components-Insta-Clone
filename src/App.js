753620c93585740fad4d79aa41ba1ba5566ec005
Cassius-Cassity <80988165+Cassius-Cassity@users.noreply.github.com>
2021-10-05 20:21:13 -0600

    finished MVP

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
+import Posts from './components/Posts/Posts'; // Am Supposed to import them indiviually or does refering to the file include everything?
+import SearchBar from './components/SearchBar/SearchBar';
+import DummyData from './dummy-data';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
 import './App.css';
+
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-
+const [posts, setPosts] = useState(DummyData);
   const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
@@ -27,12 +31,23 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+   const updatedLikes = posts.map(post => {
+     if (post.id === postId) {
+       return { ...post, likes: post.likes + 1}
+     } else {
+       return post;
+     }
+   })
+console.log(updatedLikes);
+   setPosts(updatedLikes);
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar />
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <Posts posts = {posts} likePost = {likePost}/>
     </div>
   );
 };
