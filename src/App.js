1e740ff7a6579e75d7bfdf2cdcc9df0edaa55cb3
quavo <qua.thesage19@gmail.com>
2021-11-02 14:01:08 -0500

    added states for app component

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ import dummyData from './dummy-data';
 import './App.css';
 
 const App = () => {
+  const [posts, setPosts] = useState(dummyData);
+
+  const [search, setSearch] = useState();
+  
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
