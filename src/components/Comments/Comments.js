6492d24b7d45da5935bca90a79810683a5bfe846
OrdainedRat <marcusjuniusbrutus213@gmail.com>
2021-11-02 18:05:33 -0500

    done I think, but troubleshooting a comment

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -4,13 +4,17 @@ import './Comments.css';
 
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
-  const { comments } = props;
+  const {comments} = props;
 
   return (
     <div>
-      {/* map through the comments prop and render a Comment for every piece of data */}
+      {/* map through the comments prop and render a Comment for every piece of data */
+        comments.map((comment) => (
+          <Comment comment={comment} key={comment.id} />
+        ))
+      }
     </div>
   );
 };
-
+ 
 export default Comments;
