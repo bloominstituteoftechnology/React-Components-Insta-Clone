589964c699f42427bdc9b3de4b45e7b6a5f3822a
Tuan147 <90153951+Tuan147@users.noreply.github.com>
2021-11-02 15:53:36 -0400

    finished all tasks

--- src/App.js
+++ src/App.js
@@ -40,14 +40,14 @@ const App = () => {
         return post;
       }
     })
-
+    setPosts(updatedLikes);
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       <SearchBar />
-      <Posts />
+      <Posts posts = {posts} likePost = {likePost}/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
 
     </div>
--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -9,8 +9,8 @@ const Comments = props => {
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
-      {comments.map(comment => {
-        return <Comment coment = {comment} />
+      { comments.map(comment => {
+        return <Comment coment = {comment} key = {comment.id}/>
       })}
     </div>
   );
