3862f58db578f3c79f981f0cd327bff22742ce7f
EstrellaDionis <EstrellaDionis@gmail.com>
2021-11-02 23:43:41 -0400

    have all tasks running

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,10 +5,13 @@ import './Comments.css';
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
-
+  console.log(comments)
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map((comment, idx) => (
+        <Comment comment={comment} key={idx} />
+      ))}
     </div>
   );
 };
