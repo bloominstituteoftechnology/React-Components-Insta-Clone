9a4d42f862688224f78fb2b8873933a4e8ea60c2
Roxanne Weber <marie.sankofa@gmail.com>
2021-11-04 14:41:19 -0500

    commit for pull

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -3,15 +3,25 @@ import Post from './Post';
 import './Posts.css';
 
 const Posts = (props) => {
-  // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+	console.log(props);
+	// 🔥 Make sure the parent of Posts is passing the right props!
+	const { likePost, posts } = props;
 
-  return (
-    <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
-      {/* Check the implementation of Post to see what props it requires! */}
-    </div>
-  );
+	return (
+		<div className='posts-container-wrapper'>
+			<h1>Here are the posts!</h1>
+
+			{/* Map through the posts array returning a Post component at each iteration */}
+			{/* Check the implementation of Post to see what props it requires! */}
+			{posts.map((post) => {
+				return (
+					<Post post={post} key={post.id} />
+				);
+			})}
+
+
+		</div>
+	);
 };
 
 export default Posts;
