2bc250e1ef51139beddc487513d67ccfff774fb9
Markeisha Wallacce <Markeisha94@gmail.com>
2021-12-07 13:53:58 -0600

    finished all components except commentsjs

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -9,6 +9,9 @@ const Comments = props => {
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map((comment, index) => (
+        <Comment comment={comment} key={index} />
+      ))}
     </div>
   );
 };
--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -10,20 +10,20 @@ const Post = props => {
   return (
     <div className='post-border'>
       <PostHeader
-        username={post.username}
-        thumbnailUrl={post.thumbnailUrl}
+        username={props.username}
+        thumbnailUrl={props.thumbnailUrl}
       />
       <div className='post-image-wrapper'>
         <img
           alt='post thumbnail'
           className='post-image'
-          src={post.imageUrl}
+          src={props.imageUrl}
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      <LikeSection likePost={() => likePost(props.id)} numberOfLikes={props.likes} />
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments comments={props.comments} />
     </div>
   );
 };
