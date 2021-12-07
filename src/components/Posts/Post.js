ab5c385a198ad089d8bc693ddc56f01f28d88d7a
lomelo_x <aalomelo@gmail.com>
2021-12-07 15:28:38 -0600

    Rendered LikSection and Comments to Post.js

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -4,7 +4,6 @@ import LikeSection from './LikeSection';
 import PostHeader from './PostHeader';
 
 const Post = props => {
-  // 🔥 Make sure the parent of Post is passing the right props!
   const { post, likePost } = props;
 
   return (
@@ -20,10 +19,8 @@ const Post = props => {
           src={post.imageUrl}
         />
       </div>
-      {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
-      {/* Comments also wants its props! */}
-      <Comments />
+      <LikeSection likePost={() => likePost(post.id)} numberOfLikes={post.likes} />
+      <Comments comments={post.comments} />
     </div>
   );
 };
