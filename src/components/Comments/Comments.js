1e4156a3759d6f08d4d010782be6ff60d4afb8cc
Carlos Solano <solanoandy@gmail.com>
2021-12-07 17:16:51 -0600

    MVP met

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -9,6 +9,9 @@ const Comments = props => {
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map(com => {
+        return <Comment comment={com}/>
+      })}
     </div>
   );
 };
