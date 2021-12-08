67def546dc76c36b543833ede1020052578a9520
Shamir Gibbons <nonshalant@shamirs-air.myfiosgateway.com>
2021-12-07 23:59:15 -0500

    ipdate

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,10 +5,14 @@ import './Comments.css';
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
+  // const comments = props.comments
 
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map((comment, idx) =>(
+        <Comment comment={comment} key={idx} />
+      ))}
     </div>
   );
 };
