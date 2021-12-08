6a20d5d0fa266dee6dcb4a86f6869409e2995690
marcel <marcel_rodrgez@yahoo.com>
2021-12-08 02:45:03 -0500

    Added posts from data and like functionalities

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,11 +4,14 @@ import './Posts.css';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
-
+  const { posts, likePost} = props;
+  // console.log(posts,"hello")
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map(p => {
+        return <Post post={p} numberOfLikes={p.likes} likePost={() => likePost(p.id)} key={p.id} />
+      })}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
