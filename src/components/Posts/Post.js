beb37219fbc47bc46e2496e97ad58febb720e8c5
Ahmadshah Wahidy <shah.wahidy@gmail.com>
2021-12-07 16:15:15 -0800

    completed modul attempt 1

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -21,9 +21,9 @@ const Post = props => {
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      <LikeSection likePost={() => likePost(post.id)} numberOfLikes={post.likes}/>
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments comments={post.comments} />
     </div>
   );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -9,6 +9,7 @@ const Posts = (props) => {
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map(post => <Post post={post} likePost={likePost} />)}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
