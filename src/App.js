3bc46e7fe5889dd36e46e40e44322626da43bb4f
Visnu <vgcaisse@gmail.com>
2021-12-07 21:56:57 -0500

    finished the mvp

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () => {
      */
     const updatedPosts = posts.map(post => {
       if (post.id === postId) {
-        return { ...post, likes: post.likes};
+        return { ...post, likes: post.likes + 1 };
       } else {
         return post;
       }
