e6934756e229c1f318fc6bccde103b9a3e44e6c9
vladimir <vladimirsaakian@gmail.com>
2022-01-18 18:40:58 -0800

    HW

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -5,27 +5,51 @@ import PostHeader from './PostHeader';
 
 const Post = props => {
   // 🔥 Make sure the parent of Post is passing the right props!
-  const { post, likePost } = props;
+  const { post, likePost,search } = props;
 
-  return (
-    <div className='post-border'>
-      <PostHeader
-        username={post.username}
-        thumbnailUrl={post.thumbnailUrl}
-      />
-      <div className='post-image-wrapper'>
-        <img
-          alt='post thumbnail'
-          className='post-image'
-          src={post.imageUrl}
+  const check = (search) => {
+    if (search.length === 0) {
+       return true;
+    } else {
+       if (post.username.search(search) >= 0) {
+          return true;
+       }
+       for (let i of post.comments) {
+          if (i.username.search(search) >= 0 || i.text.search(search) >= 0) {
+             return true;
+          }
+       }
+       return false;
+    }
+ };
+
+ return (
+  check(search) && (
+     <div className="post-border">
+        {/* {console.log(check(search))} */}
+        <PostHeader
+           username={post.username}
+           thumbnailUrl={post.thumbnailUrl}
         />
-      </div>
-      {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
-      {/* Comments also wants its props! */}
-      <Comments />
-    </div>
-  );
+        <div className="post-image-wrapper">
+           <img
+              alt="post thumbnail"
+              className="post-image"
+              src={post.imageUrl}
+           />
+        </div>
+        {/* Is LikeSection getting all the props it needs to work correctly? */}
+        <LikeSection
+           likePost={() => likePost(post.id)}
+           numberOfLikes={post.likes}
+           postID={post.id}
+        />
+        {/* Comments also wants its props! */}
+        <Comments comments={post.comments} />
+     </div>
+  )
+);
 };
 
 export default Post;
+
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,12 +4,20 @@ import './Posts.css';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+  const { likePost, posts,search } = props;
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {posts.map((post) => (
+            <Post
+               key={post.id}
+               post={post}
+               likePost={likePost}
+               search={search}
+            ></Post>
+         ))}
     </div>
   );
 };
