0f0c2e9abee66df03901849c3bcf9b3739455c35
Joshmclemore <josh_mclemore@yahoo.com>
2022-02-15 17:00:25 -0600

    Added additional data. Finished with attempt 1

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -54,6 +54,36 @@ const dummyData = [
             },
         ],
     },
+    {
+        id: 3,
+        username: "IalwaysBeHiking",
+        thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
+        imageUrl: 'https://images.unsplash.com/photo-1501555088652-021faa106b9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGlraW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1600&q=60',
+        likes: 2,
+        timestamp: "June 12th 2021, 03:30:00 pm",
+        comments: [
+            {
+                id: 22,
+                username: "stylzbyjoe",
+                text: "Love the view!",
+            },
+            {
+                id: 30,
+                username: "boondocksaint43",
+                text: "Where is this?",
+            },
+            {
+                id: 18,
+                username: "200LeaguesUnderTheBriegh",
+                text: "Fake",
+            },
+            {
+                id: 62,
+                username: "MaggieMaybeItsMaybeline",
+                text: "Was this with the new camera we got you?",
+            },
+        ],
+    },
 ];
 
 export default dummyData;
