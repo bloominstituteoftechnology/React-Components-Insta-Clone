b30563d4e2581aa8fdcccb9a0dce283515f0cfa4
Joshmclemore <josh_mclemore@yahoo.com>
2022-02-15 16:19:59 -0600

    Finished all tasks

--- src/App.js
+++ src/App.js
@@ -32,11 +32,16 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-    // posts.map(post => {
-    //   if (post.id === id) {
-    //     return { ...post}
-    //   }
-    //   })
+    const updatedPosts = posts.map(post => {
+      if (post.id === postId) {
+        return { ...post, likes: post.likes + 1}
+      } else {
+        return post;
+      }
+    })
+
+    setPosts(updatedPosts)
+
   };
 
   return (
