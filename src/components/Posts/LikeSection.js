bd5ed4c69354ff0ab90a36cf1761614eeea30ef8
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2022-03-22 21:32:29 -0400

    Completed SearchBar Components

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -1,7 +1,8 @@
-// Look at the number of likes on line 26. Right now it's hard coded to '100'.
+// Look at the number of likes on line 27. Right now it's hard coded to '100'.
 // Use a piece of data coming in through props to display the correct number of likes.
-// You will also add an onClick handler that utilizes `likePost` to increase the count of likes.
-// (As a stretch goal, you might want to prevent your user from "liking" the same post more than once.)
+// You will also add an onClick handler that utilizes `likePost` to increase the 
+// count of likes. (As a stretch goal, you might want to prevent your user from
+// "liking" the same post more than once.)
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
@@ -16,14 +17,14 @@ const LikeSection = props => {
         className='like-section'
         key='likes-icons-container'
       >
-        <div className='like-section-wrapper'>
+        <div className='like-section-wrapper' onClick={likePost}>
           <FontAwesomeIcon icon={faHeart} />
         </div>
         <div className='like-section-wrapper'>
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>100 likes</p>
+      <p className='like-number'>{numberOfLikes} likes</p>
     </div>
   );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -10,6 +10,9 @@ const Posts = (props) => {
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      { posts.map(post => {
+        return <Post post={post} likePost={likePost} key={post.id}/>
+      })}
     </div>
   );
 };
