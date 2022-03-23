4619313caf7d77f7fbaa3ffd4ffd401689af1b3e
Kohlten Bills <kohltenbillsy@gmail.com>
2022-03-23 09:49:54 -0600

    completed

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -6,26 +6,26 @@ import PostHeader from './PostHeader';
 const Post = props => {
   // 🔥 Make sure the parent of Post is passing the right props!
   const { post, likePost } = props;
-
+  console.log(props)
   return (
-    <div className='post-border'>
-      <PostHeader
-        username={post.username}
-        thumbnailUrl={post.thumbnailUrl}
-      />
-      <div className='post-image-wrapper'>
-        <img
-          alt='post thumbnail'
-          className='post-image'
-          src={post.imageUrl}
+      <div className='post-border'>
+        <PostHeader
+          username={props.username}
+          thumbnailUrl={props.thumbnailUrl}
         />
+        <div className='post-image-wrapper'>
+          <img
+            alt='post thumbnail'
+            className='post-image'
+            src={post.imageUrl}
+          />
+        </div>
+        {/* Is LikeSection getting all the props it needs to work correctly? */}
+        <LikeSection likePost={() => likePost(post.id)} numberOfLikes={post.likes} />
+        {/* Comments also wants its props! */}
+        <Comments comments={post.comments}/>
       </div>
-      {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
-      {/* Comments also wants its props! */}
-      <Comments />
-    </div>
-  );
-};
+    );
+  };
 
-export default Post;
+  export default Post;
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,12 +4,17 @@ import './Posts.css';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+    console.log(props)
+    const { posts, likePost } = props;
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {posts.map(post => {
+        console.log(post)
+        return <Post post={post} likePost={likePost} key={post.id} />
+      })}
     </div>
   );
 };
