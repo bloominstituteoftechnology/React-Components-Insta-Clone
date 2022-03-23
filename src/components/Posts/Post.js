39278cb766dde228f52ad975d936c815b68bfbc7
Jericho-West <97463076+Jericho-West@users.noreply.github.com>
2022-03-22 22:17:29 -0400

    test

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -5,19 +5,22 @@ import PostHeader from './PostHeader';
 
 const Post = props => {
   // 🔥 Make sure the parent of Post is passing the right props!
-  const { post, likePost } = props;
+
+  const {post, likePost} = props;
+
+  console.log(props)
 
   return (
     <div className='post-border'>
-      <PostHeader
+      <PostHeader 
         username={post.username}
-        thumbnailUrl={post.thumbnailUrl}
+        thumbnailUrl={post.thumbnailUrl} 
       />
       <div className='post-image-wrapper'>
         <img
           alt='post thumbnail'
           className='post-image'
-          src={post.imageUrl}
+          src={post.imageUrl} 
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,10 +5,12 @@ import './Posts.css';
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
+  console.log(props)
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {posts}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
