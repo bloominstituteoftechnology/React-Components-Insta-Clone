17c92391b145d629274caf7497bbb230b6eb8934
RealSadLemon <esmodearburk@gmail.com>
2022-05-17 14:39:21 -0500

    Mapped both posts and comments.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from './components/Posts/Posts.js';
 // Import the dummyData
+import dummyData from './dummy-data.js';
 import './App.css';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts, setPosts] = useState(dummyData);
+  const [search, setSearch] = useState('');
 
   const likePost = postId => {
     /*
@@ -33,6 +37,7 @@ const App = () => {
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <Posts posts={posts} likePost={likePost} />
     </div>
   );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,10 +5,15 @@ import './Posts.css';
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
+  
 
   return (
     <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
+      {/* Map through the posts array returning a Post component at each iteration */
+        posts.map(post =>{
+          return <Post likePost={likePost} post={post} key={post.id} />
+        })
+      }
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
