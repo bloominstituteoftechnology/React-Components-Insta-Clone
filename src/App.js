95fa6556eccbc4c73a3d05f8c8dd7b9398ead476
JordonP123 <jordonaren@gmail.com>
2022-07-19 23:03:14 -0400

    Done with MVP

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
+import SearchBar from './components/SearchBar/SearchBar';
+import Posts from './components/Posts/Posts';
+import PostHeader from './components/Posts/PostHeader';
+import Post from './components/Posts/Post';
+import LikeSection from './components/Posts/LikeSection';
+import dummyData from './dummy-data';
 import './App.css';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts, setPosts] = useState(dummyData);
 
   const likePost = postId => {
     /*
@@ -27,12 +34,23 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    const likes = posts.map(e => {
+      if(e.id === postId){
+        return {...e, likes: e.likes + 1}
+      }
+      else{
+        return e
+      }
+    })
+    setPosts(likes);
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <SearchBar/>
+      <Posts posts={posts} likePost={likePost}/>
     </div>
   );
 };
--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,10 +5,12 @@ import './Comments.css';
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
-
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map(comment =>{
+      return <Comment comment={comment} key={comment.id} />
+      })}
     </div>
   );
 };
