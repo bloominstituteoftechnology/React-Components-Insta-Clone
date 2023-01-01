1034f250dda229405c7a4ee4a5056a81a6bf004a
Anthony Climer <anthonyclimer0925@gmail.com>
2022-12-31 20:43:16 -0500

    All the gp stuff are done

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, {useState} from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import posts from "./components/Posts/Posts";
+import SearchBar from "./components/SearchBar/SearchBar";
 // Import the dummyData
+import dummyData from './dummy-data';
 import './App.css';
+import Post from './components/Posts/Post';
+import Posts from './components/Posts/Posts';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-
+const [posts, setPosts] = useState(dummyData); 
   const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
@@ -26,11 +31,22 @@ const App = () => {
       The callback passed into `map` performs the following logic:
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
-     */
+        */
+
+    const updatePosts = posts.map(post =>{
+      if(post.id === postId){
+        return {...post,  likes: post.likes + 1}
+      } else {
+        return post;
+      }
+    })
+  setPosts(updatePosts);
   };
 
   return (
     <div className='App'>
+      <SearchBar/>
+      <Posts posts={posts} likePost={likePost} />
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
