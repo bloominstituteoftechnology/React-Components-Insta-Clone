465539cda63cdbdb20662c5fdcbdd3aaf09d146f
DJ1 <jimmynoble604@gmail.com>
2023-02-07 19:41:02 -0500

    comments.js complete

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -9,6 +9,9 @@ const Comments = props => {
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      { comments.map(comment => {
+        return <Comment comment={comment} key={comment.id} />
+      })}
     </div>
   );
 };
