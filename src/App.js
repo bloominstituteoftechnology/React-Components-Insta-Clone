b3377c57a2cc499b0979106928960e1d87453d9a
Christian Anderson <tehninjawaffle@gmail.com>
2023-07-07 04:27:30 -0700

    Referenced the video, didn't even know where to start. I vaguely kinda understand what I just did, have serious doubts I could replicate it though.

--- src/App.js
+++ src/App.js
@@ -32,8 +32,15 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    const updatedPosts = posts.map(post => {
+      if (post.id === postId) {
+        return  { ...post, likes: post.likes + 1 }
+      } else {
+        return post;
+      }
+    }) 
     
-    return 
+    setPosts(updatedPosts);
   };
   
 //aa
