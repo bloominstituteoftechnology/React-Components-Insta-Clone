94588b77405092f3d875de40440321916ad27450
Christian Anderson <tehninjawaffle@gmail.com>
2023-07-07 02:38:28 -0700

    Imported Posts, SearchBar, and dummyData

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from "./components/Posts/Posts";
+import SearchBar from "./components/SearchBar/SearchBar";
 // Import the dummyData
+import dummyData from "./dummy-data";
 import './App.css';
 
 const App = () => {
